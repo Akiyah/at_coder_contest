3a1cc9ab962cfd05c3e2e28159aceef1018d4cdf
Akiyah <akiya.mizukoshi@gmail.com>
2024-01-30 02:28:29 +0900

    d ac

--- abc338/work/main.rb
+++ abc338/work/main.rb
@@ -1,37 +1,58 @@
 N, M = gets.chomp.split.map(&:to_i)
 XS = gets.chomp.split.map(&:to_i).map { |i| i - 1}
 
-data = Array.new(N, 0)
-
-l_min = 0
-(M - 1).times do |i|
-  #pp "---"
-  #pp i
-  x = XS[i]
-  y = XS[(i + 1) % N]
+cost_min = 0
+cost_diff = Array.new(N, 0)
+cost_total = Array.new(N, 0)
+(1...M).each do |i|
+  #pp "---#{i}---"
+  x = XS[i - 1]
+  y = XS[i]
   #pp [x, y]
 
-  x, y = [x, y].min, [x, y].max
-  #pp [x, y]
+  x, y = y, x if y < x
 
-  l = (y - x).abs
-  #pp l
+  l = y - x # 0 < l
   if N - l < l
-    x, y = y, x + N
-  end
-  #pp [x, y]
-  #pp [x % N, y % N]
+    # y -> (N - 1) -> 0 -> x と、N番目の橋を渡る場合
+    d = N - l
+    d2 = (N - d) - d
+    #pp [x, y, d, d2]
+    #pp cost_diff
+    cost_diff[y] += d2
+    #pp cost_diff
+    if x > 0
+      #pp "if x > 0"
+      cost_diff[0] += d2
+      cost_diff[x] += -d2
+    end
 
-  d = y - x
-  l_min += d
-  d2 = (N - d) - d
-  (x...y).each do |j|
-    data[j % N] += d2
+    cost_min += d
+  else
+    # x -> y と、N番目の橋を渡らない場合
+    d = l
+    d2 = (N - d) - d
+    cost_diff[x] += d2
+    cost_diff[y] += -d2
+
+    cost_min += d
   end
-  #pp data
+
+  #pp cost_min
+  #pp cost_diff
 end
 
-#pp l_min
-#pp data
+#pp cost_min
+#pp cost_diff
 
-puts l_min + data.min
+N.times do |j|
+  if j == 0
+    cost_total[j] = cost_min + cost_diff[j]
+  else
+    cost_total[j] = cost_total[j - 1] + cost_diff[j]
+  end
+end
+# pp cost_total
+
+
+puts cost_total.min
