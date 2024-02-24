8ea82a864562f0a071744e601cee76c459969a71
Akiyah <akiya.mizukoshi@gmail.com>
2024-02-24 10:40:47 +0900

    work

--- abc340/work/main.rb
+++ abc340/work/main.rb
@@ -7,46 +7,50 @@ end
 MIN_COST = []
 MIN_COST[1] = 0
 
+VS_CALCED = []
+VS_CALCING = []
 
-# a step
-(1..N).each do |i|
-  a, b, x = ABXS[i - 1]
+VS_CALCING << [1, 0] # index, cost
 
-  if i < N
-    MIN_COST[i + 1] = MIN_COST[i] + a
+
+def calc
+  loop do
+    pp [VS_CALCED.count, VS_CALCING.count]
+    #unless
+    # VS_CALCED.include? { |v| v[0] == N }
+    v = VS_CALCING.min { |v| v[1] }
+    i, cost = v
+
+    if i == N
+      return cost
+    end
+
+    a, b, x = ABXS[i - 1]
+
+    v2 = VS_CALCING.find { |v| v[0] == i + 1 }
+    if v2
+      if cost + a < v2[1]
+        v2[1] = cost + a
+      end
+    else
+      VS_CALCING << [i + 1, cost + a]
+    end
+
+    v3 = VS_CALCING.find { |v| v[0] == x }
+    if v3
+      if cost + b < v3[1]
+        v3[1] = cost + b
+      end
+    else
+      VS_CALCING << [x, cost + b]
+    end
+
+    VS_CALCING.delete(v)
+    VS_CALCED << v
   end
 end
 
-# b step
-(1..N).each do |i|
-  a, b, x = ABXS[i - 1]
-
-  if 1 < i
-    MIN_COST[i] = MIN_COST[i - 1] + a
-  end
-end
 
 
-# def calc(i, cost)
-#   # pp [i, cost]
-#   # pp MIN_COST
-#   if i == N
-#     if MIN_COST[i] && MIN_COST[i] < cost
-#     else
-#       MIN_COST[i] = cost
-#     end
-#     return 0
-#   end
 
-#   if MIN_COST[i] && MIN_COST[i] < cost
-#   else
-#     MIN_COST[i] = cost
-#     a, b, x = ABXS[i - 1]
-#     calc(i + 1, cost + a)
-#     calc(x, cost + b)
-#   end
-# end
-
-calc(1, 0)
-#pp MIN_COST
-puts MIN_COST[-1]
+puts calc
