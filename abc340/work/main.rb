8c098225751abcc6c06b1d2bf4df39acec3cd2cf
Akiyah <akiya.mizukoshi@gmail.com>
2024-02-24 12:44:22 +0900

    update

--- abc340/work/main.rb
+++ abc340/work/main.rb
@@ -4,27 +4,18 @@ ABXS = (1...N).map do
   gets.chomp.split.map(&:to_i)
 end
 
-MIN_COST = []
-MIN_COST[1] = 0
-
 VS_CALCED = []
 VS_CALCING = []
 
-VS_CALCING << [1, 0] # index, cost
+VS_CALCING[1] = 0 # index, cost
 
 
 def calc
   loop do
-    # pp "*" * 50
-    # pp [VS_CALCED.count, VS_CALCING.count]
-    # pp VS_CALCED
-    # pp VS_CALCING
-    #unless
-    # VS_CALCED.include? { |v| v[0] == N }
-    #v = VS_CALCING.min { |v| v[1] }
-    v = VS_CALCING.min { |a, b| a[1] <=> b[1] }
-    # pp v
-    i, cost = v
+    # v = VS_CALCING.min { |a, b| a[1] <=> b[1] }
+    min_value = VS_CALCING.compact.min
+    i = VS_CALCING.find_index(min_value)
+    cost = VS_CALCING[i]
 
     if i == N
       return cost
@@ -32,26 +23,24 @@ def calc
 
     a, b, x = ABXS[i - 1]
 
-    v2 = VS_CALCING.find { |v| v[0] == i + 1 }
-    if v2
-      if cost + a < v2[1]
-        v2[1] = cost + a
+    unless VS_CALCED[i + 1]
+      if VS_CALCING[i + 1]
+        VS_CALCING[i + 1] = [VS_CALCING[i + 1], cost + a].min
+      else
+        VS_CALCING[i + 1] = cost + a
       end
-    else
-      VS_CALCING << [i + 1, cost + a] unless VS_CALCED.index(i + 1)
     end
 
-    v3 = VS_CALCING.find { |v| v[0] == x }
-    if v3
-      if cost + b < v3[1]
-        v3[1] = cost + b
+    unless VS_CALCED[x]
+      if VS_CALCING[x]
+        VS_CALCING[x] = [VS_CALCING[x], cost + b].min
+      else
+        VS_CALCING[x] = cost + b
       end
-    else
-      VS_CALCING << [x, cost + b] unless VS_CALCED.index(x)
     end
 
-    VS_CALCING.delete(v)
-    VS_CALCED << v[0]
+    VS_CALCING[i] = nil
+    VS_CALCED[i] = true
   end
 end
 
