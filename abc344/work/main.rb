898343438d94e9a3944a46fd41bf51bd18be4c48
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-12 20:49:55 +0900

    abc344 update

--- abc344/work/main.rb
+++ abc344/work/main.rb
@@ -6,51 +6,35 @@ N = gets.chomp.to_i
 SS = (1..N).map do
   gets.chomp.split[1..-1].sort_by(&:size).reverse
 end
-
-pp SS
-
-max_ss_sizes = SS.map do |ss|
-  ss.map(&:size).max
-end
-
-pp max_ss_sizes
-
-REMAIN_MAX_SS_SIZES = (0...N).map do |i|
-  max_ss_sizes[i..-1].sum
-end
-
-pp REMAIN_MAX_SS_SIZES
-
 # pp SS
 
-$min_cost = N + 1
-
-def calc(t, d, cost)
-  return if d == N
-  return if $min_cost <= cost + 1
-  return if REMAIN_MAX_SS_SIZES[d] < t.size
-  pp [d, cost, $min_cost, t]
-
-  SS[d].each do |s|
-    if t == s
-      $min_cost = cost + 1
-    else
-      if t.start_with?(s)
-        calc(t[s.size..-1], d + 1, cost + 1)
-      # else
-      #   calc(t, d + 1, cost)
+def calc_by_d(dp, d)
+  dp_new = Array.new(T.length + 1)
+  dp.each_with_index do |cost, ti|
+    next unless cost
+    # 1 円を支払い、袋 i からちょうどひとつの文字列を選択して S の末尾に連結する。
+    SS[d].each do |s|
+      if T[ti..-1].start_with?(s)
+        ti_new = ti + s.length
+        dp_new[ti_new] = [(dp_new[ti_new] || 1000), cost + 1].min
       end
     end
+
+    # 何もしない。
+    dp_new[ti] = [(dp_new[ti] || 1000), cost].min
   end
-  calc(t, d + 1, cost)
+
+  dp_new
 end
 
-
-calc(T, 0, 0)
-
-if $min_cost == N + 1
-  puts -1
-else
-  puts $min_cost
+def calc()
+  dp = Array.new(T.length + 1)
+  dp[0] = 0
+  (0...N).each do |d|
+    dp = calc_by_d(dp, d)
+  end
+  dp[T.length]
 end
-#pp results.min
+
+result = calc()
+puts (result || -1)
