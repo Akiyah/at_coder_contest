1ff99c41ae56b11a5163da27d0a3176b761f1035
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-15 01:54:52 +0900

    add input3.txt

--- arc173/work/create_input.rb
+++ arc173/work/create_input.rb
@@ -1,25 +1,8 @@
-N = 200000
-Q = 200000
+N = 100
 
-puts [N, Q].join(' ')
+puts N
 
-AS = (1..Q).map do
-  rand(1000) + 1
-end
-
-puts AS.join(' ')
-
-(1..Q).each do
-  t = rand(2) + 1
-  if t == 1
-    p = rand(N) + 1
-    x = rand(1000) + 1
-    puts [1, p, x].join(' ')
-  else
-    l = rand(N) + 1
-    r = rand(N) + 1
-    l, r = r, l if r < l
-    puts [2, l, r].join(' ')
-  end
+(1..N).each do
+  puts rand(1000000000000) + 1
 end
 
