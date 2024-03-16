344d81107b099a7e0e4caa2f1b0b57c31673bd96
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-16 17:10:44 +0900

    tyoical90 001 ac * 17, tle * 12

--- typical90/work/main.rb
+++ typical90/work/main.rb
@@ -4,17 +4,35 @@ N, L = gets.chomp.split.map(&:to_i)
 K = gets.chomp.to_i
 AS = gets.chomp.split.map(&:to_i)
 
-# pp AS.combination(K).to_a
+# pp [N, L, K, AS]
 
-result = AS.combination(K).map do |as|
-  # pp as
-  as = ([0] + as + [L])
-  # pp "as"
-  # pp as
-  # pp "(0...K).map { |i| as[i + 1] - as[i] }"
-  # pp (0..K).map { |i| as[i + 1] - as[i] }
-  (0..K).map { |i| as[i + 1] - as[i] }.min
-end.max
+def check(d)
+  m = 0
+  e = 0
+  last_a = 0
+  AS.each do |a|
+    e += a - last_a
+    # pp "e: #{e}"
+    last_a = a
+    if d <= e
+      # pp "d <= e: #{d} <= #{e}"
+      e = 0
+      m += 1
+      if K == m
+        return d <= L - a
+      end
+    end
+  end
+  false
+end
 
-# pp result
-puts result
+def calc
+  (1..L).each do |d|
+  # [13, 14].each do |d|
+    r = check(d)
+    # pp [d, r]
+    return d - 1 unless r
+  end
+end
+
+puts calc
