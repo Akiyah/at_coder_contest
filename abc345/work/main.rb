894ea57e458b294464ed44a224e47e25b7e580d1
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-17 15:08:26 +0900

    refactoring

--- abc345/work/main.rb
+++ abc345/work/main.rb
@@ -6,13 +6,42 @@ ABS = (1..N).map { gets.chomp.split.map(&:to_i) }.sort_by { |a, b| -a * b }
 
 Z_FULL = 2 ** (H * W) - 1
 
+XS = (0..H).map do |a|
+  (0..W).map do |b|
+    (0...b).sum do |j|
+      (2 ** a - 1) << j * H
+    end
+  end
+end
+# pp XS
+
 def check_a_b(a, b, abs, z)
   # pp ['check_a_b', a, b, abs, z.to_s(2)]
-  x = (0...b).sum do |j|
-    (2 ** a - 1) << j * H
-  end
+  # x = (0...b).sum do |j|
+  #   (2 ** a - 1) << j * H
+  # end
+  x = XS[a][b]
+  #pp [x, x2] unless x == x2
+
+  # z_a = z.to_s(2).split('')
+  # pp z
+  # pp z.to_s(2)
+  # pp z_a
+  # pp z_a.length
+  # pp H * W - z_a.length
+  # z_a = z_a + [0] * (H * W - z_a.length)
+  # z_a.each_with_index do |d, k|
+  #   next if d != 0
+  #   next if H * W < k + (a * W + b)
+  #   y = (x << k)
+  #   if (y & z) == 0
+  #     return true if check_ab(abs, y | z)
+  #   end  
+  # end
+
   (0..(H - a)).each do |i|
     (0..(W - b)).each do |j|
+      # next if z_a[j * H + i] == 1
       y = (x << (j * H + i))
       if (y & z) == 0
         return true if check_ab(abs, y | z)
@@ -29,10 +58,6 @@ def check_ab(abs, z)
 
   a, b = abs[0]
 
-  # return false if abs.sum { |a2, b2| a2 * b2 } < H * W - z.to_s(2).split('').count('1')
-
-
-  # if a * b <= H * W - z.to_s(2).split('').count('1')
   # 回転なし
   return true if check_a_b(a, b, abs[1..-1], z)
 
@@ -40,12 +65,7 @@ def check_ab(abs, z)
   if a != b
     return true if check_a_b(b, a, abs[1..-1], z)
   end
-
-  # このタイルを使わない
-  # check_ab(abs[1..-1], z)
 end
-
-
 
 def check
   z = 0 # board 2 digit H x W
