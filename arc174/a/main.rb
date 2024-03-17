8351b416de4087bf6817b1d2e9617f959f6e8699
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-18 01:05:53 +0900

    refactoring

--- arc174/a/main.rb
+++ arc174/a/main.rb
@@ -14,54 +14,28 @@ l = nil
 m_l = 0
 m_r = 0
 # 総和が正で最大の区間を探す C <= 0 の場合は負で最小の区間を探す
-if 0 < C
-  (0...N).each do |i|
-    a = AS[i]
+(0...N).each do |i|
+  a = (0 < C ? AS[i] : -AS[i])
 
-    if sum == nil
-      if 0 < a # 区間の始まり
-        l = i
-        sum = a
-        m = sum if m < sum
-      end
-    else
-      sum += a
-      if m < sum # 区間の終了の更新
-        m = sum
-        m_l = l
-        m_r = i
-      end
-      if sum <= 0 # 区間を探すのをやめる
-        sum = nil
-      end
+  if sum == nil
+    if 0 < a # 区間の始まり
+      l = i
+      sum = a
+      m = sum if m < sum
     end
-
-    pp [i, a, sum, l, m, m_l, m_r] if $debug
-  end
-else
-  (0...N).each do |i|
-    a = AS[i]
-
-    if sum == nil
-      if a < 0 # 区間の始まり
-        l = i
-        sum = a
-        m = sum if sum < m
-      end
-    else
-      sum += a
-      if sum < m # 区間の終了の更新
-        m = sum
-        m_l = l
-        m_r = i
-      end
-      if 0 <= sum # 区間を探すのをやめる
-        sum = nil
-      end
+  else
+    sum += a
+    if m < sum # 区間の終了の更新
+      m = sum
+      m_l = l
+      m_r = i
     end
-
-    pp [i, a, sum, l, m, m_l, m_r] if $debug
+    if sum <= 0 # 区間を探すのをやめる
+      sum = nil
+    end
   end
+
+  pp [i, a, sum, l, m, m_l, m_r] if $debug
 end
 
 pp [m, m_l, m_r] if $debug
@@ -73,5 +47,5 @@ result0 = AS.sum
 if 0 < C
   puts result0 + m * (C - 1)
 else
-  puts result0 + m * (C - 1)
+  puts result0 - m * (C - 1)
 end
