1ee80b1e8f1ec458cbde645f67ca376c18bef1c4
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-21 02:55:08 +0900

    typical90 a AC

--- typical90/work/main.rb
+++ typical90/work/main.rb
@@ -27,6 +27,35 @@ def check(d)
 end
 
 def calc
+  d0 = 1
+  d1 = L / K + 1
+  r0 = check(d0) # true
+  r1 = check(d1) # false
+
+  return d0 if d1 - d0 == 1
+
+  loop do
+    # pp [d0, r0, d1, r1]
+    d = (d0 + d1) / 2
+
+    r = check(d)
+
+    if r
+      d0 = d
+      #d1 = d1
+      r0 = r
+      #r1 = r1
+    else
+      d0 = d0
+      d1 = d
+      #r0 = r0
+      r1 = r
+    end
+
+    return d0 if d1 - d0 == 1
+  end
+
+
   (1..L).each do |d|
   # [13, 14].each do |d|
     r = check(d)
