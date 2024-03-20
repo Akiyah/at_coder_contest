d37a8d18008edb031f8816425a38d3d25e532ed4
Akiyah <akiya.mizukoshi@gmail.com>
2024-03-20 21:13:40 +0900

    add input14

--- abc345/work/run.sh
+++ abc345/work/run.sh
@@ -11,6 +11,7 @@ ruby main.rb < input10.txt > actual/actual10.txt
 ruby main.rb < input11.txt > actual/actual11.txt
 ruby main.rb < input12.txt > actual/actual12.txt
 ruby main.rb < input13.txt > actual/actual13.txt
+ruby main.rb < input14.txt > actual/actual14.txt
 
 diff actual/actual1.txt expected1.txt
 diff actual/actual2.txt expected2.txt
@@ -25,5 +26,6 @@ diff actual/actual10.txt expected10.txt
 diff actual/actual11.txt expected11.txt
 diff actual/actual12.txt expected12.txt
 diff actual/actual13.txt expected13.txt
+diff actual/actual14.txt expected14.txt
 
 
