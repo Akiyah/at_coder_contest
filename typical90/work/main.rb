2b8f0c5af69632ed1a574c887fa41d87075d5e0e
Akiyah <akiya.mizukoshi@gmail.com>
2024-05-13 03:08:41 +0900

    wip

--- typical90/work/main.rb
+++ typical90/work/main.rb
@@ -17,24 +17,29 @@ end
 
 pp paths if $debug
 
-
-
 def create_depth_leafs(paths)
+  up_paths = {}
+  down_paths = {}
+
   parent_child_nodes = [[nil, 1]]
   depth_leafs = {}
 
   depth = 0
   while !parent_child_nodes.empty?
+    pp [parent_child_nodes, depth_leafs] if $debug
     next_parent_child_nodes = []
 
     parent_child_nodes.each do |parent_node, child_node|
       descendant_nodes = paths[child_node] - [parent_node]
+
       if descendant_nodes.empty?
         depth_leafs[depth] ||= []
         depth_leafs[depth] << child_node
       else
+        down_paths[child_node] = descendant_nodes
         descendant_nodes.each do |descendant_node|
           next_parent_child_nodes << [child_node, descendant_node]
+          up_paths[descendant_node] = child_node
         end
       end    
     end
@@ -43,33 +48,34 @@ def create_depth_leafs(paths)
     depth += 1
   end
 
-  depth_leafs
+  [depth_leafs, up_paths, down_paths]
 end
 
-depth_leafs = create_depth_leafs(paths)
-
+depth_leafs, up_paths, down_paths = create_depth_leafs(paths)
 
 pp depth_leafs if $debug
+pp up_paths if $debug
+pp down_paths if $debug
 
 
-distances = Array.new(N)
-max_distance = 0
-depth_leafs.keys.sort.each do |depth|
-  depth_leafs[depth].each do |leaf|
-    # 親に登っていく
-    parend_child_nodes = [leaf, nil]
-    next_parend_child_nodes = []
-    d = 0
-    while !parend_child_nodes.empty?
+# distances = Array.new(N)
+# max_distance = 0
+# depth_leafs.keys.sort.each do |depth|
+#   depth_leafs[depth].each do |leaf|
+#     # 親に登っていく
+#     parend_child_nodes = [leaf, nil]
+#     next_parend_child_nodes = []
+#     d = 0
+#     while !parend_child_nodes.empty?
 
-      parend_child_nodes.each do |parent_node, child_node|
-        distances[parent_node] = d
-      end
+#       parend_child_nodes.each do |parent_node, child_node|
+#         distances[parent_node] = d
+#       end
 
-      parend_child_nodes = next_parend_child_nodes
-      d += 1
-    end
+#       parend_child_nodes = next_parend_child_nodes
+#       d += 1
+#     end
 
-    puts leaf
-  end
-end
+#     puts leaf
+#   end
+# end
