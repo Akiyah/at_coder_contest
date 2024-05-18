0ef906c6fc1d990d0bf1c653ade44c845cf68229
Akiyah <akiya.mizukoshi@gmail.com>
2024-05-18 20:09:16 +0900

    learning

--- typical90/work/main.rb
+++ typical90/work/main.rb
@@ -1,4 +1,5 @@
 # require "ac-library-rb/segtree"
+require 'matrix'
 
 $debug = !ARGV[0].nil?
 
@@ -7,8 +8,42 @@ CS = STDIN.gets.chomp.split.map(&:to_i)
 
 M = 10 ** 9 + 7
 
-r = CS.repeated_permutation(N).count do |cs|
-  cs.map.with_index { |c, i| c * (10 ** i) }.sum % B == 0
+
+def calc_range
+  rs = []
+  B.times do |b|
+    r = (10 ** b) % B
+    a = rs.find_index(r)
+    return [a, b] if a
+    rs << r
+  end
 end
 
-puts r % M
+r0, r1 = calc_range
+pp calc_range if $debug
+
+if $debug
+  pp (0...r0)
+  pp (r0...r1)
+  d = r1 - r0
+  c = (N - r0) / d
+  s = c * d
+  pp (s...N)
+
+end
+
+
+m = Matrix.unit(B)
+N.times do |i|
+  # pp [i, (10 ** i) % B] if $debug
+  m1 = Matrix.zero(B)
+  CS.each do |c|
+    c2 = c * (10 ** i)
+    B.times do |b|
+      m1[(c2 + b) % B, b] += 1
+    end
+  end
+  m *= m1
+end
+
+puts m[0, 0] % M
