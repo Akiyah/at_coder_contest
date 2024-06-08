72cdaf2cc0c716dac3cf4bcd9182ee0d137985a2
Akiyah <akiya.mizukoshi@gmail.com>
2024-06-08 20:01:06 +0900

    refsctoring

--- arc179/work/main.rb
+++ arc179/work/main.rb
@@ -1,75 +1,40 @@
-# require "ac-library-rb/priority_queue"
-# require "ac-library-rb/segtree"
-# require "ac-library-rb/dsu"
-
 $debug = !ARGV[0].nil?
-
-# N = STDIN.gets.chomp.to_i
-# N, A, X, Y = STDIN.gets.chomp.split.map(&:to_i)
-# AS = (1..N).map do
-#   STDIN.gets.chomp.to_i
-#   STDIN.gets.chomp.split.map(&:to_i)
-# end
-
-# inf = (1 << 60) - 1
-# seg_max = AcLibraryRb::Segtree.new(rs, -inf) { |x, y| [x, y].max }
 
 M, N = STDIN.gets.chomp.split.map(&:to_i)
 XS = STDIN.gets.chomp.split.map(&:to_i)
 
 R = 998244353
 
-rxs = Hash.new { |hash, key| hash[key] = [] }
-XS.zip(1..).each do |x, i|
-  rxs[x] << i
-end
 
-pp rxs if $debug
-
-iss = (0...(2 ** M)).map do |ds|
-  ds.digits(2).zip(1..M).select { |d, i| d != 0 }.map { |d, i| i }
-end
-
-# pp iss if $debug
-
-def is2n(is)
-  is.sum { |i| 2 ** (i - 1) }
-end
-
-def n2is(n)
-  n.digits(2).zip(1..).select { |d, i| 0 < d }.map { |d, i| i }.uniq.sort
-end
-
-$n_2_n2 = {}
-iss.each do |is|
-  $n_2_n2[is2n(is)] ||= {}
-  is.each do |i|
-    $n_2_n2[is2n(is)][i] = is2n((is - [i] + rxs[i]).uniq)
+def create_nss(xs, m)
+  rxs = Array.new(M, 0)
+  xs.each.with_index do |x, i|
+    rxs[x - 1] += (1 << i)
   end
-end
 
-# pp $n_2_n2 if $debug
-
-def count(rxs, iss)
-  dp = Array.new(2 ** M, 0)
-  dp[is2n(1..M)] = 1 # first status
-  # pp dp if $debug
-
-  (1..N).each do |i|
-    pp i if $debug
-    dp_next = Array.new(2 ** M, 0)
-    (0...(2 ** M)).each do |n|
-      c = dp[n]
-      $n_2_n2[n].each do |i, n2|
-        dp_next[n2] = (dp_next[n2] + c) % R
-        #dp_next[n2] %= R
-      end
+  nss = Array.new(1 << m) { [] }
+  (1 << m).times do |n|
+    n.digits(2).each.with_index do |d, i|
+      next if d == 0
+      n2 = n & ~(1 << i) | rxs[i]
+      nss[n2] << n
     end
-    dp = dp_next
-    # pp dp if $debug
   end
-
-  dp.sum % R
+  nss
 end
 
-puts count(rxs, iss)
+def count(rs, m, r)
+  nss = create_nss(rs, m)
+  dp = Array.new(1 << m, 0)
+  dp[(1 << m) - 1] = 1 # first status
+
+  N.times do |i|
+    dp = nss.map { |ns| ns.sum { |n| dp[n] } }
+    if i % 12 == 0
+      dp = dp.map { |d| d % r }
+    end
+  end
+  dp.sum % r
+end
+
+puts count(XS, M, R)
