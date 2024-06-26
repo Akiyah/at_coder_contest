ad742bb554e5cd2c08004438730160e63873536d
Akiyah <akiya.mizukoshi@gmail.com>
2024-06-27 01:19:18 +0900

    main.rb -> main_simple.rb

--- typical90/work/main_simple.rb
+++ typical90/work/main_simple.rb
@@ -4,21 +4,20 @@
 $debug = !ARGV[0].nil?
 
 N = STDIN.gets.chomp.to_i
-S = STDIN.gets.chomp
-
-R = 10 ** 9 + 7
-
-def calc(ss, as)
-  if as.length == 0
-    return 1
-  end
-
-  a = as[0]
-  ss.map.with_index do |s, i|
-    next 0 unless s == a
-    calc(ss[i..-1], as[1..-1])
-  end.sum % R
+XYS = (1..N).map do
+  STDIN.gets.chomp.split(' ').map(&:to_i)
 end
+pp XYS if $debug
 
-puts calc(S.split(''), 'atcoder'.split(''))
+r = (0...N).to_a.permutation(3).map do |p1, p2, p3|
+  pp [p1, p2, p3, XYS[p1], XYS[p2], XYS[p3]] if $debug
+  ax = XYS[p1][0] - XYS[p2][0]
+  ay = XYS[p1][1] - XYS[p2][1]
+  bx = XYS[p3][0] - XYS[p2][0]
+  by = XYS[p3][1] - XYS[p2][1]
+  cos = (ax * bx + ay * by).to_f / Math.sqrt((ax ** 2 + ay ** 2) * (bx ** 2 + by ** 2))
+  pp [[ax, ay], [bx, by], cos, Math.acos(cos)] if $debug
+  Math.acos(cos) / Math::PI * 180
+end.max
 
+puts r.to_i == r ? r.to_i : r
