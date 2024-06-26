dd1ec313b26b30b8d0fce4c117330e52a86b9fd0
Akiyah <akiya.mizukoshi@gmail.com>
2024-06-27 01:17:46 +0900

    create simple logic

--- typical90/work/main.rb
+++ typical90/work/main.rb
@@ -4,28 +4,20 @@
 $debug = !ARGV[0].nil?
 
 N = STDIN.gets.chomp.to_i
-S = STDIN.gets.chomp
-
-R = 10 ** 9 + 7
-
-def calc
-  ss = S.split('')
-  as = 'atcoder'.split('')
-
-  rs = Array.new(as.length + 1, 0)
-  rs[0] = 1
-  pp rs if $debug
-
-  ss.each do |s|
-    i = as.find_index(s)
-    if i
-      rs[i + 1] += rs[i]
-      rs[i + 1] %= R
-    end
-    pp rs if $debug
-  end
-
-  rs[-1] % R
+XYS = (1..N).map do
+  STDIN.gets.chomp.split(' ').map(&:to_i)
 end
+pp XYS if $debug
 
-puts calc
+r = (0...N).to_a.permutation(3).map do |p1, p2, p3|
+  pp [p1, p2, p3, XYS[p1], XYS[p2], XYS[p3]] if $debug
+  ax = XYS[p1][0] - XYS[p2][0]
+  ay = XYS[p1][1] - XYS[p2][1]
+  bx = XYS[p3][0] - XYS[p2][0]
+  by = XYS[p3][1] - XYS[p2][1]
+  cos = (ax * bx + ay * by).to_f / Math.sqrt((ax ** 2 + ay ** 2) * (bx ** 2 + by ** 2))
+  pp [[ax, ay], [bx, by], cos, Math.acos(cos)] if $debug
+  Math.acos(cos) / Math::PI * 180
+end.max
+
+puts r.to_i == r ? r.to_i : r
