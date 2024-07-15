eafcd310b92155e682bb4b76f53c3217ed059398
Akiyah <akiya.mizukoshi@gmail.com>
2024-07-15 16:01:15 +0900

    typical90 011 ac

--- typical90/work/main.rb
+++ typical90/work/main.rb
@@ -13,18 +13,26 @@ pp DCSS if $debug
 dcss = DCSS.sort_by { |d, c, s| d }
 pp dcss if $debug
 
-def calc(dcss, day = 0)
-  return 0 if dcss.length == 0
+def calc(dcss)
 
-  d, c, s = dcss[0]
+  dp = Hash.new(0)
+  dp[0] = 0
 
-  s1 = 0
-  if day + c <= d # 今回のjobを行える
-    s1 = s + calc(dcss[1..-1], day + c) # 今回のjobを行った場合
+  dcss.each do |d, c, s|
+    dp_new = Hash.new(0)
+
+    dp.each do |day_count, score|
+      dp_new[day_count] = [dp_new[day_count], score].max
+      if day_count + c <= d
+        dp_new[day_count + c] = [dp_new[day_count + c], score + s].max
+      end
+    end
+
+    dp = dp_new
   end
-  s2 = calc(dcss[1..-1], day) # 今回のjobを行わなかった場合
 
-  return [s1, s2].max
+
+  return dp.values.max
 end
 
 puts calc(dcss)
