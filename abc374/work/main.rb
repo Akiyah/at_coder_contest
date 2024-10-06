8ef9db6b88e6a53631929b2f80accb23950e18fa
Akiyah <akiya.mizukoshi@gmail.com>
2024-10-06 15:01:56 +0900

    abc374 e wa

--- abc374/work/main.rb
+++ abc374/work/main.rb
@@ -17,38 +17,50 @@ APBQS = (1..N).map do
 end
 
 
-APBQS.each do |a, p, b, q|
-  
+
+def calc_w_i(w, i)
+  a, p, b, q = APBQS[i]
+
+  w0 = w / (a * b)
+  w1 = w % (a * b)
+
+  s0 = w0 * [b * p, a * q].min
+
+  s1 = (0..b).map do |k| # Aの個数
+    if w1 <= k * a
+      k * p
+    else
+      w2 = w1 - k * a
+      j = (w2 % b == 0 ? w2 / b : w2 / b + 1)
+      # pp ['**', [w, w0, w1], i, [k, j], [a, p, b, q], p * i + q * j] if $debug
+      k * p + j * q
+    end
+  end.min
+
+  pp ['*', [w, w0, w1], i, [a, p, b, q], [s0, s1, s0 + s1]] if $debug
+
+  s0 + s1
 end
 
-
-
-
-m = (0...N).to_a.permutation.map do |ps|
-  calc(ps)
-end.min
-
-
-def check_one(w, i)
-  
-end
-
-def check(w)
-  (0...N).all? { |i| check_one(w, i) }
+def calc_w(w)
+  (0...N).sum { |i| calc_w_i(w, i) }
 end
 
 def calc
   w0 = 0 # 初期状態：成立
-  w1 = 10 ** 7 # 初期状態：不成立
+  w1 = 10 ** 9 # 初期状態：不成立のはず
+
+  return w1 if calc_w(w1) <= X
 
   while w1 - w0 > 1
     w = (w0 + w1) / 2
-
-    if check(w) # 上げる
+    x = calc_w(w)
+    if x <= X
       w0 = w
-    else # 下げる
+    else
       w1 = w
     end
+    pp [[w0, w1, w], x, X, x <= X] if $debug
   end
 
   w0
@@ -56,4 +68,4 @@ end
 
 
 
-puts m
+puts calc
