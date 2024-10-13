7062e490d6632c3f1d5b600dd1407196315eb6a0
Akiyah <akiya.mizukoshi@gmail.com>
2024-10-13 20:28:37 +0900

    add main_tle

--- abc375/work/main_tle.rb
+++ abc375/work/main_tle.rb
@@ -21,7 +21,7 @@ def calc
   return -1 unless s % 3 == 0
 
 
-  dp = { 0 => { 0 => 0}}
+  dp = { [0, 0] => 0 }
   pp dp if $debug
   ABS.each do |a, b|
     dp_new = {}
@@ -30,13 +30,10 @@ def calc
       x2 = (a2 == 1 ? b : 0)
       y2 = (a2 == 2 ? b : 0)
       # z2 = (a2 == 3 ? 1 : 0)
-      dp.each do |x, dp_x|
-        dp_x.each do |y, c|
-          if x + x2 <= s / 3 && y + y2 <= s / 3
-            dp_new[x + x2] ||= {}
-            d = dp_new[x + x2][y + y2]
-            dp_new[x + x2][y + y2] = (d ? [c + c2, d].min : c + c2)
-          end
+      dp.each do |(x, y), c|
+        if x + x2 <= s / 3 && y + y2 <= s / 3
+          d = dp_new[[x + x2, y + y2]]
+          dp_new[[x + x2, y + y2]] = (d ? [c + c2, d].min : c + c2)
         end
       end
     end
@@ -45,7 +42,7 @@ def calc
     pp dp if $debug
   end
 
-  (dp[s / 3] && dp[s / 3][s / 3]) || -1
+  dp[[s / 3, s / 3]] || -1
 end
 
 puts calc
