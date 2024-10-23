49010946940b9516366fba5981fdaa0dea6b5f49
Akiyah <akiya.mizukoshi@gmail.com>
2024-10-24 02:28:33 +0900

    abc376 f ac

--- abc376/work/main.rb
+++ abc376/work/main.rb
@@ -37,38 +37,54 @@ def move_plus(t, l, r) # lをプラス方向に動かす
 end
 
 dp = {}
-dp[[0, 1]] = 0
-# pp dp if $debug
+dp[1] = 0
+h_last = 'L'
+t_last = 0
 
 NTS.each do |h, t|
   dp_next = {}
   if h == 'L'
-    dp.each do |(l, r), c|
+    dp.each do |l_r, c|
+      if h_last == 'L'
+        l = t_last
+        r = l_r
+      else
+        l = l_r
+        r = t_last
+      end
+
       # プラス方向
       l2, r2, c2 = move_plus(t, l, r)
-      lr2 = [l2, r2]
-      dp_next[lr2] = c + c2 if !dp_next[lr2] || c + c2 < dp_next[lr2]
+      dp_next[r2] = c + c2 if !dp_next[r2] || c + c2 < dp_next[r2]
 
       # マイナス方向
       l2, r2, c2 = move_plus(N - t, N - l, N - r)
-      lr2 = [N - l2, N - r2]
-      dp_next[lr2] = c + c2 if !dp_next[lr2] || c + c2 < dp_next[lr2]
+      dp_next[N - r2] = c + c2 if !dp_next[N - r2] || c + c2 < dp_next[N - r2]
     end
   else # h == 'R'
-    dp.each do |(l, r), c|
+    dp.each do |l_r, c|
+      if h_last == 'L'
+        l = t_last
+        r = l_r
+      else
+        l = l_r
+        r = t_last
+      end
+
       # プラス方向
       r2, l2, c2 = move_plus(t, r, l)
-      lr2 = [l2, r2]
-      dp_next[lr2] = c + c2 if !dp_next[lr2] || c + c2 < dp_next[lr2]
+      dp_next[l2] = c + c2 if !dp_next[l2] || c + c2 < dp_next[l2]
 
       # マイナス方向
       r2, l2, c2 = move_plus(N - t, N - r, N - l)
-      lr2 = [N - l2, N - r2]
-      dp_next[lr2] = c + c2 if !dp_next[lr2] || c + c2 < dp_next[lr2]
+      dp_next[N - l2] = c + c2 if !dp_next[N - l2] || c + c2 < dp_next[N - l2]
     end
   end
 
   dp = dp_next
+  h_last = h
+  t_last = t
+  
   puts dp.size if $debug
 end
 
