29073bbd83fa49813e32aa75f4d5d9a18a27096d
Akiyah <akiya.mizukoshi@gmail.com>
2025-01-14 02:26:27 +0900

    abc388 e ac

--- abc388/e/main.rb
+++ abc388/e/main.rb
@@ -23,28 +23,14 @@ N = STDIN.gets.chomp.to_i
 AS = STDIN.gets.chomp.split.map(&:to_i)
 
 
-def calc(i, j, useds, d)
-  pp ('  ' * d + {i:, j:, d:}.to_s) if $debug
-  rs = []
-  (i...N).each do |i2|
-    next if useds[i2]
-    j2 = (j...N).bsearch { |j_| AS[i2] * 2 <= AS[j_] }
-    break unless j2
-
-    (j2...N).each do |j3|
-      useds[j3] = true
-      rs << 1 + calc(i2 + 1, j3 + 1, useds, d + 1)
-      useds[j3] = false
-    end
-  end
-
-  if rs.length == 0
-    pp ('  ' * d + {i:, j:, d:}.to_s + ", 0") if $debug
-    return 0
-  end
-
-  pp ('  ' * d + {i:, j:, d:}.to_s + ", #{rs.max}") if $debug
-  rs.max
+def check(n)
+  (0...n).all? { |i| AS[i] * 2 <= AS[N - n + i] }
 end
 
-puts calc(0, 1, [], 0)
+def calc
+  r = (1..(N / 2)).bsearch { |n| !check(n) }
+
+  r ? (r - 1) : (N / 2)
+end
+
+puts calc
