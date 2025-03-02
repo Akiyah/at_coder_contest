f61dc3201983effc0f571e11a162a61cc30fd6a6
Akiyah <akiya.mizukoshi@gmail.com>
2025-03-03 00:02:11 +0900

    abc395 f ac

--- abc395/f/main.rb
+++ abc395/f/main.rb
@@ -21,6 +21,37 @@ UDS = (1..N).map do
   STDIN.gets.chomp.split.map(&:to_i)
 end
 
-m = UDS.map { |u, d| u + d }.min
+pp({N:, X:, UDS:}) if $debug
 
-puts UDS.sum { |u, d| u + d - m }
+
+def check(h)
+  x_min = 0
+  x_max = h
+  UDS.each do |u, d|
+    x_min = [0, x_min - X, h - d].max
+    x_max = [h, x_max + X, u].min
+    return false if x_max < x_min
+  end
+
+  true
+end
+
+
+def calc
+  ud_min = UDS.map { |u, d| u + d }.min
+  pp ud_min.class if $debug
+
+  ud_sum = UDS.map { |u, d| u + d }.sum
+  pp ud_sum.class if $debug
+
+
+
+  h = (1..ud_min).bsearch do |h|
+    !check(h)
+  end
+
+  h ||= ud_min + 1
+  return ud_sum - N * (h - 1)
+end
+
+puts calc
