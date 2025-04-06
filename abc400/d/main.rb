e5cf1b892e050f5ff6c322bc7aa5fc7e56dc26fd
Akiyah <akiya.mizukoshi@gmail.com>
2025-04-06 14:53:45 +0900

    refact

--- abc400/d/main.rb
+++ abc400/d/main.rb
@@ -30,7 +30,7 @@ def build_next(board, pq, l, (i, j), (di, dj))
   return unless 0 <= i1 && i1 < H && 0 <= j1 && j1 < W
     
   if SS[i1][j1] # '.'
-    if board[i1][j1] == nil || l < board[i1][j1]
+    if l < board[i1][j1]
       board[i1][j1] = l
       pq.push([l, i1, j1])
     end
@@ -38,14 +38,14 @@ def build_next(board, pq, l, (i, j), (di, dj))
   end
 
   # '#'
-  if board[i1][j1] == nil || l + 1 < board[i1][j1]
+  if l + 1 < board[i1][j1]
     board[i1][j1] = l + 1
     pq.push([l + 1, i1, j1])
   end
 
   return unless 0 <= i2 && i2 < H && 0 <= j2 && j2 < W
 
-  if board[i2][j2] == nil || l + 1 < board[i2][j2]
+  if l + 1 < board[i2][j2]
     board[i2][j2] = l + 1
     pq.push([l + 1, i2, j2])
   end
@@ -56,7 +56,7 @@ def calc()
   pq = AcLibraryRb::PriorityQueue.new {|x, y| x[0] < y[0] }
   pq.push([0, A - 1, B - 1])
 
-  board = (1..H).map { [] }
+  board = Array.new(H) { Array.new(W, 10000) }
   board[A - 1][B - 1] = 0
 
   while !pq.empty?
@@ -64,7 +64,7 @@ def calc()
     pp board if $debug
     l, i, j = pq.pop
 
-    if board[i][j] && board[i][j] < l
+    if board[i][j] < l
       next
     end
 
@@ -73,7 +73,7 @@ def calc()
     build_next(board, pq, l, [i, j], [ 0, -1])
     build_next(board, pq, l, [i, j], [ 0,  1])
 
-    if board[C - 1][D - 1] && board[C - 1][D - 1] <= l
+    if board[C - 1][D - 1] <= l
       return board[C - 1][D - 1]
     end
   end
