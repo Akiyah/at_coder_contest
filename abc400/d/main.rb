996415e3ad875724443511f2c506638e515501e7
Akiyah <akiya.mizukoshi@gmail.com>
2025-04-06 14:37:14 +0900

    refactoring

--- abc400/d/main.rb
+++ abc400/d/main.rb
@@ -23,6 +23,35 @@ end
 
 A, B, C, D = STDIN.gets.chomp.split.map(&:to_i)
 
+def build_next(board, pq, l, (i, j), (di, dj))
+  i1, j1 = i + di, j + dj
+  i2, j2 = i1 + di, j1 + dj
+
+  return unless 0 <= i1 && i1 < H && 0 <= j1 && j1 < W
+    
+  if SS[i1][j1] == '.'
+    if board[i1][j1] == nil || l < board[i1][j1]
+      board[i1][j1] = l
+      pq.push([l, i1, j1])
+    end
+    return
+  end
+
+  # SS[i1][j1] == '#'
+  if board[i1][j1] == nil || l + 1 < board[i1][j1]
+    board[i1][j1] = l + 1
+    pq.push([l + 1, i1, j1])
+  end
+
+  return unless 0 <= i2 && i2 < H && 0 <= j2 && j2 < W
+
+  if board[i2][j2] == nil || l + 1 < board[i2][j2]
+    board[i2][j2] = l + 1
+    pq.push([l + 1, i2, j2])
+  end
+end
+
+
 def calc()
   pq = AcLibraryRb::PriorityQueue.new {|x, y| x[0] < y[0] }
   pq.push([0, A - 1, B - 1])
@@ -33,184 +62,21 @@ def calc()
   while !pq.empty?
     pp pq if $debug
     pp board if $debug
-    d, i, j = pq.pop
+    l, i, j = pq.pop
 
 
-    if board[C - 1][D - 1] && board[C - 1][D - 1] <= d
+    if board[C - 1][D - 1] && board[C - 1][D - 1] <= l
       return board[C - 1][D - 1]
     end
 
-    if board[i][j] != nil && board[i][j] < d
+    if board[i][j] != nil && board[i][j] < l
       next
     end
 
-    # up
-    if 0 <= i - 2
-      if SS[i - 1][j] == '.'
-        if board[i - 1][j] == nil || d < board[i - 1][j]
-          board[i - 1][j] = d
-          pq.push([d, i - 1, j])
-        end
-        if SS[i - 2][j] == '.'
-          if board[i - 2][j] == nil || d < board[i - 2][j]
-            board[i - 2][j] = d
-            pq.push([d, i - 2, j])
-          end
-        else
-          if board[i - 2][j] == nil || d + 1 < board[i - 2][j]
-            board[i - 2][j] = d + 1
-            pq.push([d + 1, i - 2, j])
-          end
-        end
-      else
-        if board[i - 1][j] == nil || d + 1 < board[i - 1][j]
-          board[i - 1][j] = d + 1
-          pq.push([d + 1, i - 1, j])
-        end
-        if board[i - 2][j] == nil || d + 1 < board[i - 2][j]
-          board[i - 2][j] = d + 1
-          pq.push([d + 1, i - 2, j])
-        end
-      end
-    elsif 0 <= i - 1
-      if SS[i - 1][j] == '.'
-        if board[i - 1][j] == nil || d < board[i - 1][j]
-          board[i - 1][j] = d
-          pq.push([d, i - 1, j])
-        end
-      else
-        if board[i - 1][j] == nil || d + 1 < board[i - 1][j]
-          board[i - 1][j] = d + 1
-          pq.push([d + 1, i - 1, j])
-        end
-      end
-    end
-
-    # down
-    if i + 2 < H
-      if SS[i + 1][j] == '.'
-        if board[i + 1][j] == nil || d < board[i + 1][j]
-          board[i + 1][j] = d
-          pq.push([d, i + 1, j])
-        end
-        if SS[i + 2][j] == '.'
-          if board[i + 2][j] == nil || d < board[i + 2][j]
-            board[i + 2][j] = d
-            pq.push([d, i + 2, j])
-          end
-        else
-          if board[i + 2][j] == nil || d + 1 < board[i + 2][j]
-            board[i + 2][j] = d + 1
-            pq.push([d + 1, i + 2, j])
-          end
-        end
-      else
-        if board[i + 1][j] == nil || d + 1 < board[i + 1][j]
-          board[i + 1][j] = d + 1
-          pq.push([d + 1, i + 1, j])
-        end
-        if board[i + 2][j] == nil || d + 1 < board[i + 2][j]
-          board[i + 2][j] = d + 1
-          pq.push([d + 1, i + 2, j])
-        end
-      end
-    elsif i + 1 < H
-      if SS[i + 1][j] == '.'
-        if board[i + 1][j] == nil || d < board[i + 1][j]
-          board[i + 1][j] = d
-          pq.push([d, i + 1, j])
-        end
-      else
-        if board[i + 1][j] == nil || d + 1 < board[i + 1][j]
-          board[i + 1][j] = d + 1
-          pq.push([d + 1, i + 1, j])
-        end
-      end
-    end
-
-    # left
-    if 0 <= j - 2
-      if SS[i][j - 1] == '.'
-        if board[i][j - 1] == nil || d < board[i][j - 1]
-          board[i][j - 1] = d
-          pq.push([d, i, j - 1])
-        end
-        if SS[i][j - 2] == '.'
-          if board[i][j - 2] == nil || d < board[i][j - 2]
-            board[i][j - 2] = d
-            pq.push([d, i, j - 2])
-          end
-        else
-          if board[i][j - 2] == nil || d + 1 < board[i][j - 2]
-            board[i][j - 2] = d + 1
-            pq.push([d + 1, i, j - 2])
-          end
-        end
-      else
-        if board[i][j - 1] == nil || d + 1 < board[i][j - 1]
-          board[i][j - 1] = d + 1
-          pq.push([d + 1, i, j - 1])
-        end
-        if board[i][j - 2] == nil || d + 1 < board[i][j - 2]
-          board[i][j - 2] = d + 1
-          pq.push([d + 1, i, j - 2])
-        end
-      end
-    elsif 0 <= j - 1
-      if SS[i][j - 1] == '.'
-        if board[i][j - 1] == nil || d < board[i][j - 1]
-          board[i][j - 1] = d
-          pq.push([d, i, j - 1])
-        end
-      else
-        if board[i][j - 1] == nil || d + 1 < board[i][j - 1]
-          board[i][j - 1] = d + 1
-          pq.push([d + 1, i, j - 1])
-        end
-      end
-    end
-
-    # right
-    if j + 2 < W
-      if SS[i][j + 1] == '.'
-        if board[i][j + 1] == nil || d < board[i][j + 1]
-          board[i][j + 1] = d
-          pq.push([d, i, j + 1])
-        end
-        if SS[i][j + 2] == '.'
-          if board[i][j + 2] == nil || d < board[i][j + 2]
-            board[i][j + 2] = d
-            pq.push([d, i, j + 2])
-          end
-        else
-          if board[i][j + 2] == nil || d + 1 < board[i][j + 2]
-            board[i][j + 2] = d + 1
-            pq.push([d + 1, i, j + 2])
-          end
-        end
-      else
-        if board[i][j + 1] == nil || d + 1 < board[i][j + 1]
-          board[i][j + 1] = d + 1
-          pq.push([d + 1, i, j + 1])
-        end
-        if board[i][j + 2] == nil || d + 1 < board[i][j + 2]
-          board[i][j + 2] = d + 1
-          pq.push([d + 1, i, j + 2])
-        end
-      end
-    elsif j + 1 < W
-      if SS[i][j + 1] == '.'
-        if board[i][j + 1] == nil || d < board[i][j + 1]
-          board[i][j + 1] = d
-          pq.push([d, i, j + 1])
-        end
-      else
-        if board[i][j + 1] == nil || d + 1 < board[i][j + 1]
-          board[i][j + 1] = d + 1
-          pq.push([d + 1, i, j + 1])
-        end
-      end
-    end
+    build_next(board, pq, l, [i, j], [-1,  0])
+    build_next(board, pq, l, [i, j], [ 1,  0])
+    build_next(board, pq, l, [i, j], [ 0, -1])
+    build_next(board, pq, l, [i, j], [ 0,  1])
   end
 
   board[C - 1][D - 1]
