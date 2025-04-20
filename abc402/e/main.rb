3a86d6af4578e6632a2eba1f96b0aadf221e3ff3
Akiyah <akiya.mizukoshi@gmail.com>
2025-04-20 19:00:04 +0900

    abc402 e ac

--- abc402/e/main.rb
+++ abc402/e/main.rb
@@ -22,64 +22,34 @@ SCPS = (1..N).map do
   STDIN.gets.chomp.split.map(&:to_i)
 end
 
-$scps = SCPS.sort_by { |s, c, p| s * p / c }.reverse
-pp $scps if $debug
 
+def calc()
+  dp = {}
 
+  (0..X).each do |x|
+    dp[x] = Array.new(2 ** N) { 0 }
+    SCPS.each_with_index do |(s, c, p), i|
+      (0...(2 ** N)).each do |z|
+        next if z[i] == 0
 
-def calc_expected_value_i(x, qs, q_count, i) # i門を選んだ場合の期待値
-  s, c, p = $scps[i]
-
-  return 0 if x < c # 問題を解くお金を持っていない
-
-  # 不正解の期待値
-  rx = calc_expected_value(x - c, qs, q_count)
-
-  # 正解の期待値
-  qs2 = qs.dup
-  qs2[i] = true
-  ro = calc_expected_value(x - c, qs2, q_count - 1)
-
-  p0 = p / 100.0
-  rx * (1 - p0) + (ro + s) * p0
-end
-
-
-
-def calc_expected_value(x, qs, q_count) # qs: 残問題、Nこのbooleanの配列
-  pp [x, qs, q_count] if $debug
-  return 0 if q_count == 0
-
-
-  rs = []
-  N.times do |i|
-    next if qs[i] # すでにその問題はとき終わっている
-
-    s, c, p = $scps[i]
-    next if x < c # 問題を解くお金を持っていない
-
-    # 不正解の期待値
-    rx = calc_expected_value(x - c, qs, q_count)
-
-    # 正解の期待値
-    qs2 = qs.dup
-    qs2[i] = true
-    ro = calc_expected_value(x - c, qs2, q_count - 1)
-
-    p0 = p / 100.0
-    return rx * (1 - p0) + (ro + s) * p0
-
-    # rs << calc_expected_value_i(x, qs, q_count, i) # i門を選んだ場合の期待値
+        if c <= x
+          e = (p.to_f / 100) * (s + dp[x - c][z - 2 ** i])
+          e += (1 - p.to_f / 100) * dp[x - c][z]
+          dp[x][z] = e if dp[x][z] < e
+        end
+      end
+    end
   end
 
-  #rs.max # 一番期待値が高いものを返す
-  0
+  pp dp if $debug
+
+  dp[X][2 ** N - 1]
 end
 
+r = calc()
 
-qs = {}
-q_count = N
-N.times { |i| qs[i] = false } # 最初は問題を解いていない
-
-puts calc_expected_value(X, qs, q_count) # qs: 残問題、Nこのbooleanの配列
-
+if r.to_i == r
+  puts r.to_i
+else
+  puts r
+end
