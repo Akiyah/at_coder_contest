e09a3cc3570e75b8f04a6a3b70545f9fe696e6cb
Akiyah <akiya.mizukoshi@gmail.com>
2025-04-29 22:25:12 +0900

    refact

--- abc403/e/main.rb
+++ abc403/e/main.rb
@@ -23,11 +23,9 @@ end
 
 xs = []
 ys = []
-# zs = [] # yはxsのどれも接頭詞として持たない
-r = 0
 TSS.each do |t, s|
   if t == '1'
-    # xs << s # 新しい接頭詞
+    # 新しい接頭詞
     i = xs.bsearch_index { |x| s < x }
     if i
       xs = xs[0...i] + [s] + xs[i..-1]
@@ -35,29 +33,20 @@ TSS.each do |t, s|
       xs = xs + [s]
     end
 
-    ys.each do |yz|
-      y, z = yz
-      if z && y.start_with?(s)
-        yz[1] = false
-        r -= 1
-      end
-    end
+    ys = ys.select { |y| !y.start_with?(s) }
   else
-    z = xs.all? { |x| !s.start_with?(x) }
-    if z
-      r += 1
-    end
-
-    # ys << [s, z] # 新しい文字列
-    i = ys.bsearch_index { |y, z| s < y }
-    if i
-      ys = ys[0...i] + [[s, z]] + ys[i..-1]
-    else
-      ys = ys + [[s, z]]
+    if xs.all? { |x| !s.start_with?(x) }
+      # 新しい文字列
+      i = ys.bsearch_index { |y| s < y }
+      if i
+        ys = ys[0...i] + [s] + ys[i..-1]
+      else
+        ys = ys + [s]
+      end
     end
   end
   pp({xs:, ys:, t:, s:, r:}) if $debug
 
-  puts r
+  puts ys.length
 end
 
