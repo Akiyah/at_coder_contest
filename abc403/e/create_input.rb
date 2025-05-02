77c25c944aec9f1eac8f0a8b58aa80a5883b3647
Akiyah <akiya.mizukoshi@gmail.com>
2025-05-03 02:34:47 +0900

    add sample-7

--- abc403/e/create_input.rb
+++ abc403/e/create_input.rb
@@ -1,14 +1,11 @@
 chars = ('a'..'z').to_a
 
-puts 2 * 10 ** 4
+puts 2 * 10 ** 4 * 5
 
-(10 ** 4).times do |i|
-  c1 = chars.sample
-  c2 = chars.sample
-  c3 = chars.sample
-  c4 = chars.sample
-  c5 = chars.sample
+(10 ** 4 * 5).times do |i|
+  c1 = (1..4).map { chars.sample }
+  c2 = (1..6).map { chars.sample }
 
-  puts [1, [c1, c2].join('')].join(' ')
-  puts [2, [c3, c4, c5].join('')].join(' ')
+  puts [1, c1.join('')].join(' ')
+  puts [2, c2.join('')].join(' ')
 end
