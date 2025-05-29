23057a06277d793d5117d0542ba5d2d146fc83dc
Akiyah <akiya.mizukoshi@gmail.com>
2025-05-30 03:25:05 +0900

    wip

--- abc407/e/main.rb
+++ abc407/e/main.rb
@@ -17,25 +17,48 @@ $debug = !ARGV[0].nil?
 T = STDIN.gets.chomp.to_i
 
 def calc(n, as)
-  paires = (0...(2 * n - 1)).map { |i| [i, i + 1] }
-  pq = AcLibraryRb::PriorityQueue.new(paires) { |(i1, j1), (i2, j2)| as[i1] - as[j1] > as[i2] - as[j2] }
-  is = Array.new(2 * n, true)
+  paires = (0...(2 * n - 1)).map do |i|
+    { i:, j: i + 1, d: as[i] - as[i + 1], enable: true, prev: nil, next: nil }
+  end
+  paires.each_cons(2) { |pair1, pair2| pair1[:next] = pair2; pair2[:prev] = pair1 }
+  pq = AcLibraryRb::PriorityQueue.new(paires) do |pair1, pair2|
+    pair1[:d] > pair2[:d]
+  end
+  # is = Array.new(2 * n, true)
   co = 0
   r = 0
 
-  pp(pq:, is:, co:, r:) if $debug
+  pp(co:, r:) if $debug
+
+  if $debug
+    pp paires.map { |pair| [pair[:i], pair[:j], pair[:d], pair[:enable], pair[:prev] != nil, pair[:next] != nil] }
+  end
 
   while co < n
-    i, j = pq.pop
-    pp(co:, n:, r:, i:, j:, is:, pq:) if $debug
-    if is[i] && is[j]
-      is[i] = false
-      is[j] = false
-      co += 1
-      r += as[i]
-      if 0 <= i - 1 && j + 1 < 2 * n
-        pq.push([i - 1, j + 1])
-      end
+    pair = pq.pop
+    pp(co:, n:, r:, i: pair[:i], j: pair[:j]) if $debug
+    next unless pair[:enable]
+
+    co += 1
+    r += as[pair[:i]]
+
+    if pair[:prev] && pair[:next]
+      pq.push(
+        {
+          i: pair[:prev][:i],
+          j: pair[:next][:j],
+          d: pair[:prev][:d] + pair[:d] + pair[:next][:d],
+          enable: true,
+          prev: pair[:prev][:prev],
+          next: pair[:next][:next],
+        }
+      )
+    end
+    pair[:prev][:enable] = false if pair[:prev]
+    pair[:next][:enable] = false if pair[:next]
+
+    if $debug
+      pp paires.map { |pair| [pair[:i], pair[:j], pair[:d], pair[:enable], pair[:prev] != nil, pair[:next] != nil] }
     end
   end
 
@@ -44,11 +67,11 @@ end
 
 
 T.times do |t|
-  pp(t:) if $debug
   n = STDIN.gets.chomp.to_i
   as = (2 * n).times.map do
     STDIN.gets.chomp.to_i
   end
+  pp(t:, n:, as:) if $debug
   puts calc(n, as)
 end
 
