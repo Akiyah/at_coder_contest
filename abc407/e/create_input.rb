c30a1d2621d8a8092645a28e8ed4d1dc582fec5f
Akiyah <akiya.mizukoshi@gmail.com>
2025-06-03 02:32:53 +0900

    wip

--- abc407/e/create_input.rb
+++ abc407/e/create_input.rb
@@ -1,9 +1,9 @@
-T = 100
+T = 1
 puts T
 
 T.times do 
-  n = 3
-  as = (2 * n).times.map { rand(1..10) }
+  n = 2 * 10 ** 5
+  as = (2 * n).times.map { rand(1..(10 ** 9)) }
   puts n
   as.each do |a|
     puts a
--- abc407/e/main.rb
+++ abc407/e/main.rb
@@ -5,7 +5,7 @@
 # acc s 
 
 
-require "ac-library-rb/priority_queue"
+# require "ac-library-rb/priority_queue"
 require "ac-library-rb/segtree"
 # require "ac-library-rb/dsu"
 
@@ -16,50 +16,57 @@ $debug = !ARGV[0].nil?
 
 T = STDIN.gets.chomp.to_i
 
-def check(n, bs, i)
-  # pp 'check'
-  r = 0
-  (2 * n).times do |j|
-    if bs[-(j + 1)] || j == 2 * n - 1 - i
-      r += 1
-    else
-      r -= 1
-    end
-    # pp(i:, j:, r:)
-    return false if 0 < r
+def pick(aibs_sorted, bs, pq, n)
+  while true
+    aib1 = aibs_sorted.pop
+    a1, i1, b1 = aib1
+    next if bs[i1] != nil
+
+    aib2 = pq.prod(i1 + 1, 2 * n)
+    a2, i2, b2 = aib2
+    next if !i2 || bs[i2] != nil
+
+    return [aib1, aib2]
   end
-  true
 end
 
 def calc(n, as)
-  # ( : [1, 1]
-  # ) : [0, 1]
-  # segtree = AcLibraryRb::Segtree.new(2 * n, [0, 1]) do |(s1, d1), (s2, d2)|
-  #   [s1 + s2, d1 + d2]
-  # end
+  bs = Array.new(2 * n)
+  aibs = as.map.with_index { |a, i| [a, i, true]}
+  aibs_sorted = aibs.sort_by { |a, i, b| a }
 
-  bs = Array.new(2 * n, false)
-  ais = as.zip(0...(2 * n)).sort_by { |a, i| a }.reverse
+  pq = AcLibraryRb::SegTree.new(aibs, [10 ** 9 + 1, nil, false]) do |aib1, aib2|
+    a1, i1, b1 = aib1
+    a2, i2, b2 = aib2
 
-  pp(n:, as:, ais:) if $debug
+    if b1 == b2
+      a1 < a2 ? aib1 : aib2
+    else
+      b1 ? aib1 : aib2
+    end
+  end
+
+  pp(n:, bs:, aibs:, aibs_sorted:) if $debug
 
   co = 0
   r = 0
 
-  ais.each do |a, i|
-    # segtree.set(2 * n - i - 1, [1, 1])
-    # x = segtree.max_right(0) { |s, d| s * 2 <= d }
-    pp(a:, i:, r:, co:, bs:) if $debug
-    if check(n, bs, i)
-      co += 1
-      r += a
-      bs[i] = true
-      pp(a:, i:, r:, co:, bs:) if $debug
-      return r if co == n
-    else
-    end
+  while true
+    aib1, aib2 = pick(aibs_sorted, bs, pq, n)
+    a1, i1, b1 = aib1
+    a2, i2, b2 = aib2
+
+    bs[i1] = '('
+    bs[i2] = ')'
+
+    pq.set(i1, [a1, i1, false])
+    pq.set(i2, [a2, i2, false])
+
+    r += a1
+    co += 1
+
+    return r if co == n
   end
-  pp 'end'
 end
 
 
@@ -68,7 +75,7 @@ T.times do |t|
   as = (2 * n).times.map do
     STDIN.gets.chomp.to_i
   end
-  pp(t:, n:, as:) if $debug
+  # pp(t:, n:, as:) if $debug
   puts calc(n, as)
 end
 
