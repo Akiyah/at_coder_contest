6503f6ff6d52073a9590c1a82f98e5f5b7c3e623
Akiyah <akiya.mizukoshi@gmail.com>
2025-06-07 20:51:06 +0900

    abc407 e ac

--- abc407/e/main.rb
+++ abc407/e/main.rb
@@ -5,9 +5,9 @@
 # acc s 
 
 
-# require "ac-library-rb/priority_queue"
+require "ac-library-rb/priority_queue"
 # require "ac-library-rb/segtree"
-require "ac-library-rb/lazy_segtree"
+# require "ac-library-rb/lazy_segtree"
 # require "ac-library-rb/dsu"
 
 # require 'prime'
@@ -18,47 +18,17 @@ $debug = !ARGV[0].nil?
 T = STDIN.gets.chomp.to_i
 
 def calc(n, as)
-  ais = as.map.with_index { |a, i| [a, i]}
-  ais_sorted = ais.sort_by { |a, i| a }.reverse
+  r = as[0]
 
-  vector = (2 * n).times.map { |i| i - 2 * n }
-  e = - 10 ** 6
-  id = 0
+  pq = AcLibraryRb::PriorityQueue.new()
 
-  seg = AcLibraryRb::LazySegtree.new(vector, e, id) { |x, y| [x, y].max }
-  seg.set_mapping{ |f, x| f + x }
-  seg.set_composition{ |f, g| f + g }
+  as[1...-1].each_slice(2) do |a1, a2|
+    pq << a1
+    pq << a2
 
-  pp(vector:, seg:) if $debug
-
-  co = 0
-  r = 0
-  i_min = 2 * n - 1
-
-  ais_sorted.each do |a, i|
-    next if i_min <= i
-
-    # if 0 < seg.prod(0, i + 1) + 2
-    #   # i_min = i
-    #   next
-    # end
-
-    seg.range_apply(0, i + 1, 2)
-
-    if -2 < seg.prod(0, i + 1)
-      i_min = seg.max_right(0) do |x|
-        x <= -2 # x <= -2 を満たさない最初のi
-      end
-    end
-    # i_min = seg.min_left(i_min) { |v| -2 < v }
-
-    pp(a:, i:, i_min:, all_prod: seg.all_prod) if $debug
-
-    r += a
-    co += 1
-
-    return r if co == n
+    r += pq.pop
   end
+  r
 end
 
 
