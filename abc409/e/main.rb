e8d5f1d3a2de6a083ca5c5e1ebbf55bba9e5bf38
Akiyah <akiya.mizukoshi@gmail.com>
2025-06-09 01:28:24 +0900

    abc409 e ac

--- abc409/e/main.rb
+++ abc409/e/main.rb
@@ -23,3 +23,58 @@ UVWS = (1...N).map do
 end
 
 
+def calc_tree(node)
+end
+
+def calc
+  paths = {}
+  UVWS.each do |u, v, w|
+    paths[u - 1] ||= {}
+    paths[u - 1][v - 1] = w
+    paths[v - 1] ||= {}
+    paths[v - 1][u - 1] = w
+  end
+
+  pp(paths:) if $debug
+  nodes = []
+  d = 0
+  nodes[d] = []
+  nodes[d] << { current: 0, parent: nil }
+
+  while 0 < nodes[d].length
+    nodes[d + 1] = []
+
+    nodes[d].each do |node|
+      pp(node:) if $debug
+      u = node[:current]
+      parent = node[:parent]
+      paths[u].each do |v, w|
+        next if v == parent
+        
+        nodes[d + 1] << { current: v, parent: u}
+      end
+    end
+
+    d += 1
+  end
+
+  cost = 0
+  xs = XS.dup
+  nodes.reverse.each do |nodes_d|
+    nodes_d.each do |node|
+      u = node[:current]
+      parent = node[:parent]
+      if parent
+        w = paths[u][parent]
+        cost += xs[u].abs * w
+        xs[parent] += xs[u]
+      end
+    end
+  end
+
+  cost
+end
+
+
+puts calc
+
