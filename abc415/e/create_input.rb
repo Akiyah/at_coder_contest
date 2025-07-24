071a5f6c1bb291714349269957cf2b224f967de9
Akiyah <akiya.mizukoshi@gmail.com>
2025-07-25 01:59:18 +0900

    wip

--- abc415/e/create_input.rb
+++ abc415/e/create_input.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 
 
-H = 2 * 10 ** 2
-W = 10 ** 3
+# H = 2 * 10 ** 2
+# W = 10 ** 3
+
+# H = 2 * 10 ** 5
+# W = 1
+
+H = 1
+W = 2 * 10 ** 5
+
 AS = H.times.map do
   W.times.map { 1 }
 end
--- abc415/e/main.rb
+++ abc415/e/main.rb
@@ -22,40 +22,61 @@ AS = (1..H).map do
 end
 PS = STDIN.gets.chomp.split.map(&:to_i)
 
-as = AS.map.with_index do |rows, i|
-  rows.map.with_index do |a, j|
-    a - PS[i + j]
-  end
-end
 
-pp(H:, W:, AS:, PS:, as:) if $debug
+def creat_as
+  bs = AS.map.with_index do |rows, i|
+    rows.map.with_index do |a, j|
+      a - PS[i + j]
+    end
+  end
+
+  as = Array.new(H) { Array.new(W) }
+  as[0][0] = bs[0][0]
+
+  # i = 0
+  (1...W).each do |j|
+    as[0][j] = as[0][j - 1] + bs[0][j]
+  end
+
+  # j = 0
+  (1...H).each do |i|
+    as[i][0] = as[i - 1][0] + bs[i][0]
+  end
+
+  (1...H).each do |i|
+    (1...W).each do |j|
+      as[i][j] = [as[i - 1][j], as[i][j - 1]].max + bs[i][j]
+    end
+  end
+
+  as
+end
 
 def calc(as, x)
   # pp(as:, x:) if $debug
   bs = Array.new(H) { Array.new(W) } # 予約
   cs = Array.new(H) { Array.new(W) } # 答え
   bs[0][0] = true
-  cs[0][0] = as[0][0] + x
-  return false if cs[0][0] < 0
+  return false if as[0][0] + x < 0
 
   return true if H == 1 && W == 1
 
   dp = []
-  dp << [1, 0] if 1 < H
-  dp << [0, 1] if 1 < W
-
+  if 1 < H
+    dp << [1, 0]
+    bs[1][0] = true
+  end
+  if 1 < W
+    dp << [0, 1]
+    bs[0][1] = true
+  end
 
   while 0 < dp.length
     pp(dp:, cs:) if $debug
     i, j = dp.shift
-    c1 = (0 <= i - 1) ? (cs[i - 1][j] || 0) : 0
-    c2 = (0 <= j - 1) ? (cs[i][j - 1] || 0) : 0
-    c = [c1, c2].max + as[i][j]
-    next if c < 0
+    next if as[i][j] + x < 0
 
     return true if i == H - 1 && j == W - 1
-
-    cs[i][j] = c
 
     if i + 1 < H && !bs[i + 1][j]
       dp << [i + 1, j]
@@ -66,11 +87,11 @@ def calc(as, x)
       bs[i][j + 1] = true
     end
   end
-
+ 
   false
 end
 
-
+as = creat_as
 MAX_X = (10 ** 9) * (H + W - 1)
 pp(MAX_X:) if $debug
 x = (0..MAX_X).bsearch do |x|
