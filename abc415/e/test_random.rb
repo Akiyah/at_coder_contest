34163581f541199c73a6ab64b82c10a04c5a6ff9
Akiyah <akiya.mizukoshi@gmail.com>
2025-07-26 11:58:55 +0900

    add test_random

--- abc415/e/test_random.rb
+++ abc415/e/test_random.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 def create_input(h, w)
   as = h.times.map do
-    w.times.map { rand(10 ** 9 - 1) + 1 }
+    w.times.map { rand(10 ** 3 - 1) + 1 }
   end
-  ps = (h + w - 1).times.map { rand(10 ** 9 - 1) + 1 }
+  ps = (h + w - 1).times.map { rand(10 ** 3 - 1) + 1 }
 
   [h, w, as, ps]
 end
@@ -23,7 +23,7 @@ def test
   filename_input = 'test/random.in'
   filename_output_1 = 'test/random_1.out'
   filename_output_2 = 'test/random_2.out'
-  h, w, as, ps = create_input(3, 3)
+  h, w, as, ps = create_input(4, 4)
   create_input_file(h, w, as, ps, filename_input)
 
   `ruby main.rb < #{filename_input} > #{filename_output_1}`
