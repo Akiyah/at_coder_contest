1da8c6c87181fcba709db8b7104af41024254046
Akiyah <akiya.mizukoshi@gmail.com>
2025-07-26 18:12:31 +0900

    wip

--- abc415/e/main.rb
+++ abc415/e/main.rb
@@ -23,58 +23,65 @@ end
 PS = STDIN.gets.chomp.split.map(&:to_i)
 
 
-def creat_as
-  bs = H.times.map do |i|
+def creat_bs
+  H.times.map do |i|
     W.times.map do |j|
       AS[i][j] - PS[i + j]
     end
   end
-
-  as = Array.new(H) { Array.new(W) }
-  H.times.map do |i|
-    W.times.map do |j|
-      rs = []
-      rs << as[i - 1][j] if 0 <= i - 1
-      rs << as[i][j - 1] if 0 <= j - 1
-      as[i][j] = (rs.max || 0) + bs[i][j]
-    end
-  end
-
-  as
 end
 
-def calc(as, x)
-  # pp(as:, x:) if $debug
-  bs = Array.new(H) { Array.new(W) } # 予約
+def calc(bs, x)
+  pp(bs:, x:, AS:, PS:) if $debug
+  cs = Array.new(H) { Array.new(W) } # 結果
+  ds = Array.new(H) { Array.new(W) } # 予約
 
-  dp = [[0, 0]]
-  bs[0][0] = true
+  ds[0][0] = true
+  cs[0][0] = x + bs[0][0]
+  return false if cs[0][0] < 0
+  return true if 0 == H - 1 && 0 == W - 1 # goal
+
+  dp = []
+  if 1 < H
+    dp << [1, 0]
+    ds[1][0] = true
+  end
+  if 1 < W
+    dp << [0, 1]
+    ds[0][1] = true
+  end
 
   while 0 < dp.length
-    pp(dp:, cs:) if $debug
+    pp(dp:, cs:, ds:) if $debug
     i, j = dp.shift
-    next if as[i][j] + x < 0
+
+    rs = []
+    rs << cs[i - 1][j] if 0 < i && cs[i - 1][j]
+    rs << cs[i][j - 1] if 0 < j && cs[i][j - 1]
+    c = rs.max + bs[i][j]
+    next if c < 0
+    cs[i][j] = c
 
     return true if i == H - 1 && j == W - 1 # goal
 
-    if i + 1 < H && !bs[i + 1][j]
+    if i + 1 < H && !ds[i + 1][j]
       dp << [i + 1, j]
-      bs[i + 1][j] = true
+      ds[i + 1][j] = true
     end
-    if j + 1 < W && !bs[i][j + 1]
+    if j + 1 < W && !ds[i][j + 1]
       dp << [i, j + 1]
-      bs[i][j + 1] = true
+      ds[i][j + 1] = true
     end
   end
  
   false
 end
 
-as = creat_as
+bs = creat_bs
 MAX_X = (10 ** 9) * (H + W - 1)
 pp(MAX_X:) if $debug
 x = (0..MAX_X).bsearch do |x|
-  r = calc(as, x)
+  r = calc(bs, x)
   pp(x:, r:) if $debug
   r
 end
