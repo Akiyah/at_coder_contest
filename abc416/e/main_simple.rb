b42ee3c687d273dfdb79357504cd8363465127a0
Akiyah <akiya.mizukoshi@gmail.com>
2025-08-02 18:43:32 +0900

    wip

--- abc416/e/main_simple.rb
+++ abc416/e/main_simple.rb
@@ -69,102 +69,43 @@ end
 def calc_sum(paths)
   return 0 if N == 1
 
-  (0..(N - 2)).map do |i|
+  (1..(N - 1)).map do |i|
     calc_length(paths, i)
   end.sum
 end
 
-def calc_one(query, board, paths, ds)
+def calc_one(query, paths, ds)
   # pp(query:) if $debug
-  pp(query:, board:, paths:, ds:) if $debug
+  pp(query:, paths:, ds:) if $debug
   s = query[0].to_i
   if s == 1
     _, x, y, t = query
-    x = x.to_i - 1
-    y = y.to_i - 1
-    t = t.to_i
-    add_path(paths, x, y, t)
-    update_board(board, paths, x, y)
-    update_board(board, paths, y, x)
+    add_path(paths, x.to_i, y.to_i, t.to_i)
   elsif s == 2
     _, x = query
-    x = x.to_i - 1
+    x = x.to_i
     if !ds[x]
-      ds.keys.each do |d|
-        add_path(paths, d, x, T)
-        update_board(board, paths, d, x)
-        update_board(board, paths, x, d)
-      end
+      ds.keys.each { |d| add_path(paths, d, x, T) }
       ds[x] = true
     end
   else # s == 3
-    #puts calc_sum(paths)
-    puts calc_sum_board(board)
+    puts calc_sum(paths)
   end
 end
 
-def calc_sum_board(board)
-  N.times.map do |x|
-    N.times.map do |y|
-      board[x][y] || 0
-    end.sum
-  end.sum
-end
-
-def update_board(board, paths, x, y)
-  N.times do |x2|
-    next unless board[x2][x]
-    N.times do |y2|
-      next unless board[y][y2]
-      if !board[x2][y2] || board[x2][x] + paths[x][y] + board[y][y2] < board[x2][y2]
-        board[x2][y2] = board[x2][x] + paths[x][y] + board[y][y2]
-      end
-    end
-  end
-end
-
-def create_board(paths, ds)
-  board = Array.new(N) { Array.new(N) }
-
-  dp = []
-  N.times do |x|
-    board[x][x] = 0
-    dp << [x, x]
-  end
-
-  while 0 < dp.length
-    x, y = dp.shift
-
-    if paths[y]
-      paths[y].each do |y2, t2|
-        if !board[x][y2] || board[x][y] + t2 < board[x][y2] 
-          board[x][y2] = board[x][y] + t2
-          dp << [x, y2]
-        end
-      end
-    end
-  end
-
-  board
-end
 
 def calc
   paths = {}
   ABCS.each do |a, b, c|
-    add_path(paths, a - 1, b - 1, c)
+    add_path(paths, a, b, c)
   end
   ds = {}
-  DS.each { |d| ds[d - 1] = true }
-  ds.keys.combination(2) do |d1, d2|
-    add_path(paths, d1, d2, T)
-  end
-
-  board = create_board(paths, ds)
-  pp(board:) if $debug
+  DS.each { |d| ds[d] = true }
+  ds.keys.combination(2) { |d1, d2| add_path(paths, d1, d2, T) }
 
   Q.times do
     query = STDIN.gets.chomp.split
-    calc_one(query, board, paths, ds)
+    calc_one(query, paths, ds)
   end
 end
 
