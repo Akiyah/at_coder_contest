1f825d4572e36ba1520733b76d348987d89a43ce
Akiyah <akiya.mizukoshi@gmail.com>
2025-08-04 02:41:55 +0900

    abc417 d tle * 1

--- abc417/d/main.rb
+++ abc417/d/main.rb
@@ -25,100 +25,59 @@ XS = (1..Q).map do
   STDIN.gets.chomp.to_i
 end
 
-
-
-def calc_xiss(xiss, p, a, b)
-  new_xiss = []
-  merged_xis_zero = []
-  xiss.each do |h|
-    xis = h[:xis]
-    d = h[:d]
-    x = h[:x]
-    if x # すでにひとつの値に収束している場合
-      if x <= p
-        x += a
-      else
-        x -= b
-        x = 0 if x < 0
-      end
-      new_xiss << { xis:, d:, x: }
-    else # まだひとつの値に収束していない場合
-      if b < p # 0になるものがない
-        s = xis.bsearch_index { |x2, i2| p < x2 + d } # down
-        # t = xis.bsearch_index { |x2, i2| b < x2 + d } # down & 0に収束しない
-        xis_up = s ? xis[0...s] : xis
-        xis_down = s ? xis[s..] : []
-        if 0 < xis_up.length
-          if xis_up.length == 1
-            new_xiss << { xis: xis_up, d: nil, x: xis_up[0][0] + d + a}
-          else
-            new_xiss << { xis: xis_up, d: d + a, x: nil}
-          end
-        end
-        if 0 < xis_down.length
-          if xis_down.length == 1
-            new_xiss << { xis: xis_down, d: d - b, x: xis_down[0][0] + d - b}
-          else
-            new_xiss << { xis: xis_down, d: d - b, x: nil}
-          end
-        end
-      else # 0になるものがある可能性
-        s = xis.bsearch_index { |x2, i2| p < x2 + d } # down
-        t = xis.bsearch_index { |x2, i2| b < x2 + d } # down & 0に収束しない
-        xis_up = s ? xis[0...s] : xis
-        if t # tがあればsもある
-          xis_zero = xis[s...t]
-          xis_down = xis[t..]
-        else
-          xis_zero = s ? xis[s..] : []
-          xis_down = []
-        end
-        if 0 < xis_up.length
-          if xis_up.length == 1
-            new_xiss << { xis: xis_up, d: d + a, x: xis_up[0][0] + d + a}
-          else
-            new_xiss << { xis: xis_up, d: d + a, x: nil}
-          end
-        end
-        if 0 < xis_down.length
-          if xis_down.length == 1
-            new_xiss << { xis: xis_down, d: d - b, x: xis_down[0][0] + d - b}
-          else
-            new_xiss << { xis: xis_down, d: d - b, x: nil}
-          end
-        end
-        merged_xis_zero << xis_zero if 0 < xis_zero.length
-      end
-    end
-  end
-  new_xiss << { xis: merged_xis_zero.flatten(1), d: nil, x: 0 } if 0 < merged_xis_zero.length
-  new_xiss
-end
-
 def calc_xis(xis)
-  xiss = [{xis: xis, d: 0, x: nil}]
-  pp(xiss:) if $debug
+  d = 0
+  xis_by_x = {}
+
+  pp(xis:, d:, xis_by_x:) if $debug
   PABS.each do |p, a, b|
-    xiss_new = calc_xiss(xiss, p, a, b)
-    xiss = xiss_new
-    pp(xiss:, p:, a:, b:) if $debug
-  end
-  xirs = []
-  xiss.each do |h|
-    xis = h[:xis]
-    d = h[:d]
-    x = h[:x]
-    if x
-      xis.each do |x2, i2|
-        xirs << [x2, i2, x]
-      end
+    pp(p:, a:, b:) if $debug
+    m = [p, b].max
+    i = xis.bsearch_index { |x2, i2| m < x2 + d } # 十分に大きいので、個別に扱わないもの
+
+    if i
+      xis_low = xis[...i]
+      xis_high = xis[i..]
     else
-      xis.each do |x2, i2|
-        xirs << [x2, i2, x2 + d]
-      end
+      xis_low = xis
+      xis_high = []
     end
+
+    pp(i:, xis_low:, xis_high:) if $debug
+
+    xis_new = xis_high
+    xis_low.each do |x, i|
+      xis_by_x[x + d] ||= []
+      xis_by_x[x + d] << i
+    end
+    pp(xis_by_x:) if $debug
+
+    xis_by_x_new = {}
+
+    xis_by_x.each do |x, is|
+      if x <= p
+        x2 = x + a
+      else
+        x2 = x - b
+        x2 = 0 if x2 < 0
+      end
+      xis_by_x_new[x2] ||= []
+      xis_by_x_new[x2] += is
+    end
+
+    xis_by_x = xis_by_x_new
+    xis = xis_new
+    d -= b
+    pp(xis:, d:, xis_by_x:) if $debug
   end
-  xirs
+
+
+  xis_result = xis.map { |x, i| [x + d, i] }
+  xis_by_x.each do |x, is|
+    xis_result += is.map { |i| [x, i] }
+  end
+
+  xis_result.sort_by { |x, i| i }.map { |x, i| x }
 end
 
 def calc
@@ -126,12 +85,11 @@ def calc
   pp(xis:) if $debug
   xis = xis.sort_by { |x, i| x }
   pp(xis:) if $debug
-  xirs = calc_xis(xis)
-  pp(xirs:) if $debug
+  xis_result = calc_xis(xis)
+  pp(xis_result:) if $debug
 
-  xirs = xirs.sort_by { |x, i, r| i }
-  xirs.each do |x, i, r|
-    puts r
+  xis_result.each do |x, i|
+    puts x
   end
 end
 
