e54b0ce5532e2463b52bbd80907d8067f91766a2
Akiyah <akiya.mizukoshi@gmail.com>
2025-08-16 14:19:55 +0900

    wip

--- abc418/e/main.rb
+++ abc418/e/main.rb
@@ -22,70 +22,33 @@ XYS = (1..N).map do
 end
 
 
-ds = Hash.new do |hash1, key1|
-  hash1[key1] = Hash.new do |hash2, key2|
-    hash2[key2] = 0
-  end
-end
-ds_sum = Hash.new { 0 }
-# ds = Hash.new { Hash.new { Hash.new { 0 } } }
-
-# ds = Hash.new do |hash1, key1|
-#   hash1[key1] = Hash.new do |hash2, key2|
-#     hash2[key2] = 0
-#   end
-# end
-
-xys = XYS.sort
-
-r = 0
-r_minus = 0
-(0...N).each do |i|
-  x1, y1 = xys[i]
-  ((i + 1)...N).each do |j|
-    x2, y2 = xys[j]
-    # pp(x1:, y1:, x2:, y2:) if $debug
-    dx = x2 - x1
-    dy = y2 - y1
-    if dy == 0
-      t = Float::INFINITY
-    else
-      t = Rational(dx, dy)
-    end
-    g = dx.gcd(dy)
-
-    # ds[t] ||= Hash.new() { 0 }
-    if ds[t][g]
-      r_minus += ds[t][g]
-      ds[t][g] += 1
-    else
-      ds[t][g] = 1
-    end
-
-    if ds_sum[t]
-      r += ds_sum[t]
-      ds_sum[t] += 1
-    else
-      ds_sum[t] = 1
-    end
-    # ds_sum[t] ||= 0
-    # ds_sum[t] += 1
-
-    # r += ds_sum[t] - 1
-  end
+ds = {}
+XYS.sort.combination(2) do |(x1, y1), (x2, y2)|
+  # pp(x1:, y1:, x2:, y2:) if $debug
+  dx = x2 - x1
+  dy = y2 - y1
+  g = dx.gcd(dy)
+  dx /= g
+  dy /= g
+  ds[dx] ||= {}
+  ds[dx][dy] ||= {}
+  ds[dx][dy][g] ||= 0
+  ds[dx][dy][g] += 1
 end
 pp(ds: ds.sort) if $debug
 
-# r = 0
-# r_minus = 0
-# ds.each do |t, gs|
-#   c_sum = 0
-#   gs.each do |g, c|
-#     c_sum += c
-#     r_minus += c * (c - 1) / 2
-#   end
-#   r += c_sum * (c_sum - 1) / 2
-# end
+r = 0
+r_minus = 0
+ds.each do |dx, dys|
+  dys.each do |dy, gs|
+    c_sum = 0
+    gs.each do |g, c|
+      c_sum += c
+      r_minus += c * (c - 1) / 2
+    end
+    r += c_sum * (c_sum - 1) / 2
+  end
+end
 
 pp(r:, r_minus:) if $debug
 puts r - r_minus / 2
