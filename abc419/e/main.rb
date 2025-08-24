13aaf6e0dd1849a500e897ef16a5e34bf40007d8
Akiyah <akiya.mizukoshi@gmail.com>
2025-08-24 17:51:20 +0900

    wip

--- abc419/e/main.rb
+++ abc419/e/main.rb
@@ -20,17 +20,34 @@ N, M, L = STDIN.gets.chomp.split.map(&:to_i)
 AS = STDIN.gets.chomp.split.map(&:to_i)
 
 
-def calc()
-  ls = (N - L + 1).times.map do |i|
-    AS[i...(i + L)].sum % M
+def create_xs(xs_short)
+  xs = xs_short + Array.new(N - L)
+
+  xs[L - 1] = (-xs[0...(L - 1)].sum - AS[0...L].sum) % M
+
+  (L...N).each do |i|
+    xs[i] = (xs[i - L] + AS[i -  L] - AS[i]) % M
   end
 
-  pp(ls:) if $debug
+  xs
+end
 
 
+def calc()
+  ms = (0...M).to_a
+  xs_shorts = ms.product(*([ms] * (L - 2)))
+  pp(xs_shorts:) if $debug
 
+  cs = xs_shorts.map do |xs_short|
+    xs = create_xs(xs_short)
 
+    c = xs.map { |x| x % M }.sum
+    pp(xs_short: ,xs:, c:) if $debug
+    c
+  end
 
+  pp(cs:) if $debug
+  cs.min
 end
 
 
