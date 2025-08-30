b3b93e3e155478eeb1dff49ef8cb0ad6269bb0d1
Akiyah <akiya.mizukoshi@gmail.com>
2025-08-30 21:38:33 +0900

    wip

--- abc421/c/main.rb
+++ abc421/c/main.rb
@@ -16,11 +16,45 @@
 
 $debug = !ARGV[0].nil?
 
-# N = STDIN.gets.chomp.to_i
-# N, A, X, Y = STDIN.gets.chomp.split.map(&:to_i)
-# AS = (1..N).map do
-#   STDIN.gets.chomp.to_i
-#   STDIN.gets.chomp.split.map(&:to_i)
-# end
+N = STDIN.gets.chomp.to_i
+S = STDIN.gets.chomp
+
+def calc(ss, t)
+
+  as = []
+  bs = []
+
+  (0...(2*N)).each do |i|
+    if ss[i] == t
+      as << i
+    else
+      bs << i
+    end
+  end
+
+  r = 0
+  (0...(2*N)).each do |i|
+    t2 = (i % 2 == 0)
+    if t2
+      c = as[i / 2]
+    else
+      c = bs[i / 2]
+    end
+    unless c <= i
+      r += c - i
+    end
+  end
+  r
+end
+
+
+
+ss = S.chars.map { |s| s == 'A' }
+r1 = calc(ss, true)
+r2 = calc(ss, false)
+
+puts [r1, r2].min
+
+
 
 
