59c293a5ffb7575c017d897a81691f66ad08b20a
Akiyah <akiya.mizukoshi@gmail.com>
2025-09-10 23:09:16 +0900

    abc422 e tle * 2

--- abc422/e/main.rb
+++ abc422/e/main.rb
@@ -23,49 +23,65 @@ end
 
 $nn = (N / 2 + 1) * (N / 2 + 1 - 1) / 2
 
-def calc
-  rs = {}
-  XYS.combination(2).each do |(x1, y1), (x2, y2)|
-    if x2 - x1 != 0
-      r = Rational(y2 - y1, x2 - x1)
-      c = y1 - r * x1
-    else
-      r = Float::INFINITY
-      c = x1
-    end
-    rs[r] ||= {}
-    rs[r][c] ||= 0
-    rs[r][c] += 1
-    if $nn <= rs[r][c]
-      return [[x1, y1], [x2, y2]]
-    end
-  end
-  pp(rs:) if $debug
-  false
-end
-
-xys = calc
-
-if xys
-  (x1, y1), (x2, y2) = xys
-  pp(x1:, y1:, x2:, y2:) if $debug
+def calc_abc((x1, y1), (x2, y2))
   a = y2 - y1
   b = -(x2 - x1)
   c = - (b * y1) - (a * x1)
-  puts 'Yes'
-  puts [a, -b, c].join(' ')
+  [a, b, c]
+end
 
-  if $debug
-    def check(a, b, c)
-      r = XYS.count do |x, y|
-        pp(x:, y:, 'a * x + b * y + c' => a * x + b * y + c)
-        a * x + b * y + c == 0
-      end
-      N <= r * 2
+def count_xys(a, b, c)
+  XYS.count do |x, y|
+    a * x + b * y + c == 0
+  end
+end
+
+def calc(xys)
+  pp(xys:) if $debug
+
+  n = xys.length * 20 / 100 # 20%
+  pp(n:) if $debug
+
+  rcs = {}
+  abcs = []
+  xys.combination(2).each do |xy1, xy2|
+    abc = calc_abc(xy1, xy2)
+    a, b, c = abc
+    if a == 0 # y + t = 0
+      s = Float::INFINITY
+      t = Rational(c, b)
+    else # x + s * y + t = 0
+      s = Rational(b, a)
+      t = Rational(c, a)
     end
 
-    pp(a:, b:, c:)
-    pp(check: check(a, b, c))
+    rcs[s] ||= {}
+    rcs[s][t] ||= 0
+    rcs[s][t] += 1
+    if n * (n - 1) / 2 <= rcs[s][t]
+      abcs << abc
+    end
+  end
+  pp(abcs:) if $debug
+
+  abcs.each do |abc|
+    pp(abc:, count_xys: count_xys(*abc)) if $debug
+    return abc if N <= count_xys(*abc) * 2
+  end
+
+  nil
+end
+
+xys = XYS.sample(100)
+abc = calc(xys)
+
+if abc
+  puts 'Yes'
+  puts abc.join(' ')
+
+  if $debug
+    r = count_xys(*abc)
+    pp(N:, abc:, count_xys: r, check: N <= r * 2)
   end
 else
   puts 'No'
