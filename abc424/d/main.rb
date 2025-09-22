71efa7d5e94148e6aa191d8a52947c3bf2f0e081
Akiyah <akiya.mizukoshi@gmail.com>
2025-09-22 18:10:19 +0900

    use 2**n

--- abc424/d/main.rb
+++ abc424/d/main.rb
@@ -56,21 +56,38 @@ def calc_one(h, w, squares, exist_squares, effects)
   e11 = effects[(i + 1) * w + (j + 1)]
   pp(e00:, e10:, e01:, e11:) if $debug
 
-  unless e00.proper_subset?(e10) || e00.proper_subset?(e01) || e00.proper_subset?(e11)
+  # unless e00.proper_subset?(e10) || e00.proper_subset?(e01) || e00.proper_subset?(e11)
+  #   ijs << [i + 0, j + 0]
+  # end
+
+  # unless e10.subset?(e00) || e10.proper_subset?(e01) || e10.proper_subset?(e11)
+  #   ijs << [i + 1, j + 0]
+  # end
+
+  # unless e01.subset?(e00) || e01.subset?(e10) || e01.proper_subset?(e11)
+  #   ijs << [i + 0, j + 1]
+  # end
+
+  # unless e11.subset?(e00) || e11.subset?(e10) || e11.subset?(e01)
+  #   ijs << [i + 1, j + 1]
+  # end
+
+  unless ((e10 & e00) == e00 && e10 != e00) || ((e01 & e00) == e00 && e01 != e00) || ((e11 & e00) == e00 && e11 != e00)
     ijs << [i + 0, j + 0]
   end
 
-  unless e10.subset?(e00) || e10.proper_subset?(e01) || e10.proper_subset?(e11)
+  unless ((e00 & e10) == e10) || ((e01 & e10) == e10 && e01 != e10) || ((e11 & e10) == e10 && e11 != e10)
     ijs << [i + 1, j + 0]
   end
 
-  unless e01.subset?(e00) || e01.subset?(e10) || e01.proper_subset?(e11)
+  unless ((e00 & e01) == e01) || ((e10 & e01) == e01) || ((e11 & e01) == e01 && e11 != e01)
     ijs << [i + 0, j + 1]
   end
 
-  unless e11.subset?(e00) || e11.subset?(e10) || e11.subset?(e01)
+  unless ((e00 & e11) == e11) || ((e10 & e11) == e11) || ((e01 & e11) == e11)
     ijs << [i + 1, j + 1]
   end
+
 
   pp(ijs:) if $debug
 
@@ -84,8 +101,9 @@ def calc_one(h, w, squares, exist_squares, effects)
       j2 = j1 - dj
       if 0 <= i2 && 0 <= j2 && exist_squares2[i2 * w + j2]
         exist_squares2[i2 * w + j2] = false
-        effects2[i2 * w + j2] = effects2[i2 * w + j2].dup
-        effects2[i2 * w + j2].delete([i1, j1])
+        # effects2[i2 * w + j2] = effects2[i2 * w + j2].dup
+        # effects2[i2 * w + j2].delete([i1, j1])
+        effects2[i2 * w + j2] = effects2[i2 * w + j2] & (((1 << (h * w)) - 1) - (1 << (i * w + j)))
       end
     end
 
@@ -103,14 +121,14 @@ def calc
 
   squares = []
   exist_squares = Array.new(h * w, false)
-  effects = Array.new(h * w) { Set.new }
+  effects = Array.new(h * w, 0)
   (0...(h - 1)).each do |i|
     (0...(w - 1)).each do |j|
       if DIJS.all? { |di, dj| ss[i + di][j + dj] }
         squares << [i, j]
         exist_squares[i * w + j] = true
         DIJS.each do |di, dj|
-          effects[(i + di) * w + (j + dj)] << [i, j]
+          effects[(i + di) * w + (j + dj)] = effects[(i + di) * w + (j + dj)] | (1 << (i * w + j))
         end
       end
     end
