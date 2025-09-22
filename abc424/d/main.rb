0ed4f2603a37c30975d27634806e82b3d866fbc8
Akiyah <akiya.mizukoshi@gmail.com>
2025-09-22 17:54:45 +0900

    use set

--- abc424/d/main.rb
+++ abc424/d/main.rb
@@ -20,65 +20,76 @@ T = STDIN.gets.chomp.to_i
 
 DIJS = [[0, 0], [1, 0], [0, 1], [1, 1]]
 
-def calc_one(h, w, ss, squares, exist_squares, counts)
-  pp(squares:, exist_squares:, counts:) if $debug
+def calc_one(h, w, squares, exist_squares, effects)
+  pp(squares:, exist_squares:, effects:) if $debug
   return 0 if squares.length == 0
 
   i, j = squares[0]
   unless exist_squares[i * w + j] # すでに使われている
-    return calc_one(h, w, ss, squares[1..], exist_squares, counts)
+    return calc_one(h, w, squares[1..], exist_squares, effects)
   end
 
-  # m = [
-  #   counts[i * w + j],
-  #   counts[(i + 1) * w + j],
-  #   counts[i * w + j + 1],
-  #   counts[(i + 1) * w + j + 1],
-  # ].max
-  m = DIJS.map do |di, dj|
-    counts[(i + di) * w + (j + dj)]
-  end.max
+  # m = DIJS.map do |di, dj|
+  #   counts[(i + di) * w + (j + dj)]
+  # end.max
 
+  # if m == 1
+  #   ijs = [[i, j]]
+  # else
   ijs = []
-
-  if m == 1
-    ijs << [i, j]
-  else
-    # ijs << [i, j] if 1 < counts[i * w + j]
-    # ijs << [i + 1, j] if 1 < counts[(i + 1) * w + j]
-    # ijs << [i, j + 1] if 1 < counts[i * w + j + 1]
-    # ijs << [i + 1, j + 1] if 1 < counts[(i + 1) * w + j + 1]
-    DIJS.each do |di, dj|
-      ijs << [i + di, j + dj] if 1 < counts[(i + di) * w + (j + dj)]
+  zs = []
+  DIJS.each do |di, dj|
+    z = 0
+    DIJS.each_with_index do |(di2, dj2), k|
+      if 0 <= i + di - di2 && j + dj - dj2
+        if exist_squares[(i + di - di2) * w + (j + dj - dj2)]
+          z += (1 << k)
+        end
+      end
     end
+    zs << z
   end
 
-  # ijs = [[i, j], [i + 1, j], [i, j + 1], [i + 1, j + 1]]
+  e00 = effects[(i + 0) * w + (j + 0)]
+  e10 = effects[(i + 1) * w + (j + 0)]
+  e01 = effects[(i + 0) * w + (j + 1)]
+  e11 = effects[(i + 1) * w + (j + 1)]
+  pp(e00:, e10:, e01:, e11:) if $debug
+
+  unless e00.proper_subset?(e10) || e00.proper_subset?(e01) || e00.proper_subset?(e11)
+    ijs << [i + 0, j + 0]
+  end
+
+  unless e10.subset?(e00) || e10.proper_subset?(e01) || e10.proper_subset?(e11)
+    ijs << [i + 1, j + 0]
+  end
+
+  unless e01.subset?(e00) || e01.subset?(e10) || e01.proper_subset?(e11)
+    ijs << [i + 0, j + 1]
+  end
+
+  unless e11.subset?(e00) || e11.subset?(e10) || e11.subset?(e01)
+    ijs << [i + 1, j + 1]
+  end
 
   pp(ijs:) if $debug
 
   rs = []
   ijs.each do |i1, j1|
     exist_squares2 = exist_squares.dup
-    counts2 = counts.dup
+    effects2 = effects.dup
 
-    # [[i1, j1], [i1 - 1, j1], [i1, j1 - 1], [i1 - 1, j1 - 1]].each do |i2, j2|
     DIJS.each do |di, dj|
       i2 = i1 - di
       j2 = j1 - dj
       if 0 <= i2 && 0 <= j2 && exist_squares2[i2 * w + j2]
         exist_squares2[i2 * w + j2] = false
-        # counts2[i2 * w + j2] -= 1
-        # counts2[(i2 + 1) * w + j2] -= 1
-        # counts2[i2 * w + j2 + 1] -= 1
-        # counts2[(i2 + 1) * w + j2 + 1] -= 1
-        DIJS.each do |di2, dj2|
-          counts2[(i2 + di2) * w + (j2 + dj2)] -= 1
-        end
+        effects2[i2 * w + j2] = effects2[i2 * w + j2].dup
+        effects2[i2 * w + j2].delete([i1, j1])
       end
     end
 
-    rs << calc_one(h, w, ss, squares[1..], exist_squares2, counts2)
+    rs << calc_one(h, w, squares[1..], exist_squares2, effects2)
   end
 
   1 + rs.min
@@ -92,20 +103,20 @@ def calc
 
   squares = []
   exist_squares = Array.new(h * w, false)
-  counts = Array.new(h * w, 0)
+  effects = Array.new(h * w) { Set.new }
   (0...(h - 1)).each do |i|
     (0...(w - 1)).each do |j|
       if DIJS.all? { |di, dj| ss[i + di][j + dj] }
         squares << [i, j]
         exist_squares[i * w + j] = true
         DIJS.each do |di, dj|
-          counts[(i + di) * w + (j + dj)] += 1
+          effects[(i + di) * w + (j + dj)] << [i, j]
         end
       end
     end
   end
 
-  calc_one(h, w, ss, squares, exist_squares, counts)
+  calc_one(h, w, squares, exist_squares, effects)
 end
 
 
