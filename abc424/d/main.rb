611c157c0c5c28352ee0c5d64bcf5a480c89fcb3
Akiyah <akiya.mizukoshi@gmail.com>
2025-09-22 12:21:46 +0900

    wip

--- abc424/d/main.rb
+++ abc424/d/main.rb
@@ -18,6 +18,8 @@ $debug = !ARGV[0].nil?
 
 T = STDIN.gets.chomp.to_i
 
+DIJS = [[0, 0], [1, 0], [0, 1], [1, 1]]
+
 def calc_one(h, w, ss, squares, exist_squares, counts)
   pp(squares:, exist_squares:, counts:) if $debug
   return 0 if squares.length == 0
@@ -27,22 +29,28 @@ def calc_one(h, w, ss, squares, exist_squares, counts)
     return calc_one(h, w, ss, squares[1..], exist_squares, counts)
   end
 
-  m = [
-    counts[i * w + j],
-    counts[(i + 1) * w + j],
-    counts[i * w + j + 1],
-    counts[(i + 1) * w + j + 1],
-  ].max
+  # m = [
+  #   counts[i * w + j],
+  #   counts[(i + 1) * w + j],
+  #   counts[i * w + j + 1],
+  #   counts[(i + 1) * w + j + 1],
+  # ].max
+  m = DIJS.map do |di, dj|
+    counts[(i + di) * w + (j + dj)]
+  end.max
 
   ijs = []
 
   if m == 1
     ijs << [i, j]
   else
-    ijs << [i, j] if 1 < counts[i * w + j]
-    ijs << [i + 1, j] if 1 < counts[(i + 1) * w + j]
-    ijs << [i, j + 1] if 1 < counts[i * w + j + 1]
-    ijs << [i + 1, j + 1] if 1 < counts[(i + 1) * w + j + 1]
+    # ijs << [i, j] if 1 < counts[i * w + j]
+    # ijs << [i + 1, j] if 1 < counts[(i + 1) * w + j]
+    # ijs << [i, j + 1] if 1 < counts[i * w + j + 1]
+    # ijs << [i + 1, j + 1] if 1 < counts[(i + 1) * w + j + 1]
+    DIJS.each do |di, dj|
+      ijs << [i + di, j + dj] if 1 < counts[(i + di) * w + (j + dj)]
+    end
   end
 
   # ijs = [[i, j], [i + 1, j], [i, j + 1], [i + 1, j + 1]]
@@ -54,13 +62,19 @@ def calc_one(h, w, ss, squares, exist_squares, counts)
     exist_squares2 = exist_squares.dup
     counts2 = counts.dup
 
-    [[i1, j1], [i1 - 1, j1], [i1, j1 - 1], [i1 - 1, j1 - 1]].each do |i2, j2|
+    # [[i1, j1], [i1 - 1, j1], [i1, j1 - 1], [i1 - 1, j1 - 1]].each do |i2, j2|
+    DIJS.each do |di, dj|
+      i2 = i1 - di
+      j2 = j1 - dj
       if 0 <= i2 && 0 <= j2 && exist_squares2[i2 * w + j2]
         exist_squares2[i2 * w + j2] = false
-        counts2[i2 * w + j2] -= 1
-        counts2[(i2 + 1) * w + j2] -= 1
-        counts2[i2 * w + j2 + 1] -= 1
-        counts2[(i2 + 1) * w + j2 + 1] -= 1
+        # counts2[i2 * w + j2] -= 1
+        # counts2[(i2 + 1) * w + j2] -= 1
+        # counts2[i2 * w + j2 + 1] -= 1
+        # counts2[(i2 + 1) * w + j2 + 1] -= 1
+        DIJS.each do |di2, dj2|
+          counts2[(i2 + di2) * w + (j2 + dj2)] -= 1
+        end
       end
     end
 
@@ -81,13 +95,12 @@ def calc
   counts = Array.new(h * w, 0)
   (0...(h - 1)).each do |i|
     (0...(w - 1)).each do |j|
-      if ss[i][j] && ss[i + 1][j] && ss[i][j + 1] && ss[i + 1][j + 1]
+      if DIJS.all? { |di, dj| ss[i + di][j + dj] }
         squares << [i, j]
         exist_squares[i * w + j] = true
-        counts[i * w + j] += 1
-        counts[(i + 1) * w + j] += 1
-        counts[i * w + j + 1] += 1
-        counts[(i + 1) * w + j + 1] += 1
+        DIJS.each do |di, dj|
+          counts[(i + di) * w + (j + dj)] += 1
+        end
       end
     end
   end
