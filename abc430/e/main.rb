d7a03f2f4c49f04ad01bef50b4d395681ac673a0
Akiyah <akiya.mizukoshi@gmail.com>
2025-11-08 02:58:50 +0900

    abc430 e TLE

--- abc430/e/main.rb
+++ abc430/e/main.rb
@@ -16,11 +16,24 @@
 
 $debug = !ARGV[0].nil?
 
-# N = STDIN.gets.chomp.to_i
+T = STDIN.gets.chomp.to_i
 # N, A, X, Y = STDIN.gets.chomp.split.map(&:to_i)
 # AS = (1..N).map do
 #   STDIN.gets.chomp.to_i
 #   STDIN.gets.chomp.split.map(&:to_i)
 # end
 
+def calc(a, b)
+  l = a.length
+  l.times do |i|
+    return i if b == a[i..] + a[...i]
+  end
+  -1
+end
 
+T.times do
+  a = STDIN.gets.chomp
+  b = STDIN.gets.chomp
+
+  puts calc(a, b)
+end
