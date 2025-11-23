e06e4cbf5d13b9de1080ea2fb9d4c6809333a12d
Akiyah <akiya.mizukoshi@gmail.com>
2025-11-24 03:24:29 +0900

    wip

--- abc433/e/main.rb
+++ abc433/e/main.rb
@@ -29,13 +29,13 @@ def run(n, m, xs0, ys0)
   xs1 = xs0.map.with_index { |x, i| [x, i] }.sort_by {|x, i| x}.reverse
   ys1 = ys0.map.with_index { |y, i| [y, i] }.sort_by {|y, i| y}.reverse
 
-  xj2i = []
+  xi2j = []
   xs1.each.with_index do |(x, i), j|
-    xj2i[i] = j
+    xi2j[i] = j
   end
-  yj2i = []
+  yi2j = []
   ys1.each.with_index do |(y, i), j|
-    yj2i[i] = j
+    yi2j[i] = j
   end
 
   xs = xs1.map { |x, i| x}
@@ -95,14 +95,30 @@ def run(n, m, xs0, ys0)
     end
   end
 
-  puts 'Yes'
-  n.times do |i_n|
-    jn = xj2i[i_n]
-    s = m.times.map do |im|
-      jm = yj2i[im]
+  ans2 = n.times.map do |i_n|
+    jn = xi2j[i_n]
+    m.times.map do |im|
+      jm = yi2j[im]
       ans[jn][jm]
-    end.join(' ')
-    puts s
+    end
+  end
+
+  puts 'Yes'
+  ans2.each do |line|
+    puts line.join(' ')
+  end
+
+  if $debug
+    # pp(ans2:)
+    check_xs = n.times.all? do |i_n|
+      # pp(i_n:, l: xs0[i_n], r: m.times.map { |i_m| ans2[i_n][i_m] }.max)
+      xs0[i_n] == m.times.map { |i_m| ans2[i_n][i_m] }.max
+    end
+    check_ys = m.times.all? do |i_m|
+      # pp(i_m:, l: ys0[i_m], r: n.times.map { |i_n| ans2[i_n][i_m] }.max)
+      ys0[i_m] == n.times.map { |i_n| ans2[i_n][i_m] }.max
+    end
+    pp(check_xs:, check_ys:)
   end
 end
 
