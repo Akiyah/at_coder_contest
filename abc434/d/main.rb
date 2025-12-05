49a7704d4b84b6ab6dd8d3478afacb91cebc9829
Akiyah <akiya.mizukoshi@gmail.com>
2025-12-06 03:03:09 +0900

    wip

--- abc434/d/main.rb
+++ abc434/d/main.rb
@@ -21,33 +21,38 @@ UDLRS = (1..N).map do
   STDIN.gets.chomp.split.map(&:to_i)
 end
 
-dis = Array.new(2000) { [] }
-uis = Array.new(2000) { [] }
+ulis = Array.new(2000) { Array.new(2000) }
+uris = Array.new(2000) { Array.new(2000) }
+dlis = Array.new(2000) { Array.new(2000) }
+dris = Array.new(2000) { Array.new(2000) }
 UDLRS.each.with_index do |(u, d, l, r), i|
-  uis[u - 1] << i
-  dis[d - 1] << i
-end
-pp(uis: uis[...10]) if $debug
-pp(dis: dis[...10]) if $debug
+  pp(i:) if $debug
 
-lis = Array.new(2000) { Set.new }
-ris = Array.new(2000) { Set.new }
+  ulis[u - 1][l - 1] ||= Set.new
+  uris[u - 1][r - 1] ||= Set.new
+  dlis[d - 1][l - 1] ||= Set.new
+  dris[d - 1][r - 1] ||= Set.new
+
+  ulis[u - 1][l - 1] << i
+  uris[u - 1][r - 1] << i
+  dlis[d - 1][l - 1] << i
+  dris[d - 1][r - 1] << i
+end
+
 count0 = 0
 count1 = Array.new(N, 0)
 count2 = 0
-pp(lis: lis[...10], ris: ris[...10]) if $debug
+lis = Array.new(2000) { Set.new }
+ris = Array.new(2000) { Set.new }
 2000.times do |x|
-  # pp(x:) if $debug && x < 10
-  uis[x].each do |i|
-    u, d, l, r = UDLRS[i]
-    lis[l - 1] << i
-    ris[r - 1] << i
+  pp(x:) if $debug
+  2000.times do |y|
+    lis[y] += ulis[x][y] if ulis[x][y]
+    ris[y] += uris[x][y] if uris[x][y]
   end
 
   is = Set.new
-  pp(x:, is:) if $debug && x < 10
   2000.times do |y|
-    # pp(y:) if $debug && x < 10 && y < 10
     is += lis[y]
     d = is.length
 
@@ -61,16 +66,12 @@ pp(lis: lis[...10], ris: ris[...10]) if $debug
     end
 
     is -= ris[y]
-    pp(x:, y:, is:) if $debug && x < 10 && y < 10
   end
 
-  dis[x].each do |i|
-    u, d, l, r = UDLRS[i]
-    lis[l - 1].delete(i)
-    ris[r - 1].delete(i)
+  2000.times do |y|
+    lis[y] -= dlis[x][y] if dlis[x][y]
+    ris[y] -= dris[x][y] if dris[x][y]
   end
-
-  pp(lis: lis[...10], ris: ris[...10]) if $debug && x < 10
 end
 
 pp(count0:, count1:, count2:) if $debug
