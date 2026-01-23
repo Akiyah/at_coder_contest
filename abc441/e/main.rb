129d65c34e1fd1b94b34770411e8e4525156114d
Akiyah <akiya.mizukoshi@gmail.com>
2026-01-23 22:12:17 +0900

    abc441 e ac

--- abc441/e/main.rb
+++ abc441/e/main.rb
@@ -19,25 +19,70 @@ $debug = !ARGV[0].nil?
 N = STDIN.gets.chomp.to_i
 S = STDIN.gets.chomp
 
-ss = S.chars
-ns = ss.map { |x| x == 'A' ? 1 : (x == 'B' ? -1 : 0) }
-pp(ss:) if $debug
-pp(ns:) if $debug
+def calc()
 
-def calc(ns)
-  i = 0
-  j = 0
-  r = 0
-  s = 0
-  (0...N).each do |i|
-    while
-      a_count = ss[i..j].select { |x| x == 'A' }.count
-      b_count = ss[i..j].select { |x| x == 'B' }.count
-      pp(ss[i..j], a_count, b_count) if $debug
-      r += 1 if b_count < a_count
+  ss = S.chars
+  ns = ss.map { |x| x == 'A' ? 1 : (x == 'B' ? -1 : 0) }
+  hs = [0]
+  h = 0
+  ns.each do |n|
+    h += n
+    hs << h
   end
+  pp(ss:) if $debug
+  pp(ns:) if $debug
+  pp(hs:) if $debug
+
+  h_hash = hs.tally
+  pp(h_hash:) if $debug
+
+  s = h_hash.map { |h2, c2| 0 < h2 ? c2 : 0 }.sum
+  pp(s:) if $debug
+
+  r = 0
+  N.times do |i|
+    n = ns[i]
+    h = hs[i]
+    h_hash[h] -= 1
+    r += s
+    if n == 0
+    elsif n == 1
+      s -= h_hash[h + 1]
+    else # n == -1
+      s += h_hash[h]
+    end
+    # r0 = h_hash.map { |h2, c2| h < h2 ? c2 : 0 }.sum
+    pp(n:, h:, s:, r:, h_hash:) if $debug
+  end
+
+  r
 end
 
 
-puts calc(ns)
+puts calc()
+
+
+
+# ACBBCABCAB
+# A
+# AC
+#     CA
+#      A
+#      ABCA
+#     CABCA
+#        CA
+#         A
+
+#    0  2
+# A  1
+# C  1  
+# B  0  
+# B -1  2  
+# C -1  2
+# A  0
+# B -1  1  
+# C -1  1
+# A  0
+# B -1
+
 
