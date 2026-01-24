fc92a4302357f833cce4ac677ad2abee4f5f44b5
Akiyah <akiya.mizukoshi@gmail.com>
2026-01-24 13:54:02 +0900

    abc440 d ac bsearch

--- abc440/d/main.rb
+++ abc440/d/main.rb
@@ -22,75 +22,57 @@ XYS = (1..Q).map do
   STDIN.gets.chomp.split.map(&:to_i)
 end
 
-def calc(as, xyis)
-  pp(as:, xyis:) if $debug
-
-  j = 0
-  as.each.with_index do |a, i|
-    while j < Q && xyis[j][:x] <= a
-      xyis[j][:left_a_count_of_x] = i
-      xyis[j][:left_count_of_x] = (xyis[j][:x] - 1) - i
-      xyis[j][:left_count_of_y] = (xyis[j][:x] - 1) - i + (xyis[j][:y] - 1)
-      j += 1
-    end
-  end
-  while j < Q
-    xyis[j][:left_a_count_of_x] = N
-    xyis[j][:left_count_of_x] = (xyis[j][:x] - 1) - N
-    xyis[j][:left_count_of_y] = (xyis[j][:x] - 1) - N + (xyis[j][:y] - 1)
-    j += 1
-  end
-
-  # as: [1, 2, 3, 9, 16],
-  # xyis:
-  # [{x: 1, y: 1, i: 2},
-  #  {x: 6, y: 10, i: 0},
-  #  {x: 12, y: 4, i: 1},
-  #  {x: 1000000000, y: 1000000000, i: 3}]}
-
-  #  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-  #  a, a, a,                a,                          a,
-  #  0, 0, 0, 0, 1, 2, 3, 4, 5,  5,  6,  7,  8,  9, 10, 11, 11, 12, 13, 14 : left_count
-  # x0,            x1,                  x2,
-  #          y0,                                    y2,     y1,
-
-
-  xyis2 = xyis.sort_by { |xyi| xyi[:left_count_of_y] }
-
-  pp(as:, xyis:, xyis2:) if $debug
-
-  rs = []
-  j = 0
-  last_a = 0
-  as.each.with_index do |a, i|
-    pp(a:, i:) if $debug
-    left_count_of_a = a - (i + 1)
-    while j < Q && xyis2[j][:left_count_of_y] < left_count_of_a
-      pp(j:) if $debug
-      i2 = xyis2[j][:i]
-      rs[i2] = a - (left_count_of_a - xyis2[j][:left_count_of_y])
-      j += 1
-      pp(rs:) if $debug
-    end
-    last_a = a
-  end
-  pp('as loop end') if $debug
-  while j < Q
-    pp(j:) if $debug
-    i2 = xyis2[j][:i]
-    rs[i2] = xyis2[j][:left_count_of_y] + N + 1
-    j += 1
-    pp(rs:) if $debug
+def calc(as, x, y)
+  pp('---') if $debug
+  pp(as:, x:, y:) if $debug
+  s = (0..N).bsearch { |i| x <= as[i] }
+  t = (s..N).bsearch do |i|
+    pp(
+      i:,
+      a: as[i],
+      x:,
+      y:,
+      s:,
+      left: y,
+      right: as[i] - x - (i - s),
+      left_right: y <= as[i] - x - (i - s),
+    ) if $debug
+    y <= as[i] - x - (i - s)
   end
 
-  rs
+  pp(s:, t:, ans: x + y + (t - s)) if $debug
+
+  x + y + (t - s) - 1
 end
 
-as = AS.sort
-xyis = XYS.map.with_index { |(x, y), i| {x:, y:, i:} }.sort_by { |xyi| xyi[:x] }
-rs = calc(as, xyis)
+as = AS.sort + [10 ** 10]
+rs = XYS.map do |x, y|
+  calc(as, x, y)
+end
 
 rs.each do |r|
   puts r
 end
 
+# as =  [ 1, 2, 3, 9, 16, 10000000000]
+# x = 1, y = 1
+# s = 1 (x < as[s])
+
+#  i, (as[i] - x + 1) - (i - s + 1)
+#o  1, (1 - 1 + 1) - (0 - 1 + 1) = 1
+#o  2, (2 - 1 + 1) - (1 - 1 + 1) = 1
+#o  3, (3 - 1 + 1) - (2 - 1 + 1) = 1
+#   4
+#   5
+#   6
+#   7
+#   8
+#o  9, (9 - 1 + 1) - (3 - 1 + 1) = 6
+#  10
+#  11
+#  12
+#  14
+#  15
+#o 16, (16 - 1 + 1) - (4 - 1 + 1) = 12
+#  17
+#  18
