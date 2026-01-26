fefda976fd1ce32e7502039154c2d8f4570e0102
Akiyah <akiya.mizukoshi@gmail.com>
2026-01-27 01:35:24 +0900

    wip

--- abc442/e/main.rb
+++ abc442/e/main.rb
@@ -26,55 +26,45 @@ end
 
 
 def calc
-  xysrs = XYS.map do |x, y|
+  xysris = XYS.map.with_index do |(x, y), i|
     s = ((0 < x || (x == 0 && 0 < y)) ? 1 : -1)
     r = (x == 0 ? -Float::INFINITY : -Rational(y, x))
-    [x, y, s, r]
+    [x, y, s, r, i]
   end
 
-  xysrs.sort_by! { |(x, y, s, r)| [s, r] }
+  xysris.sort_by! { |(x, y, s, r, i)| [s, r] }
 
-  srs = []
-  sr_counts = Hash.new{ |hash, key| hash[key] = 0 }
+  sr2is = Hash.new{ |hash, key| hash[key] = [] }
+  sr_counts_sum = {}
   last_sr = nil
 
-  xysrs.each do |x, y, s, r|
+  xysris.each.with_index do |(x, y, s, r, i), j|
     sr = [s, r]
-    srs << sr unless last_sr == sr
-    sr_counts[sr] += 1
+    sr2is[sr] << i
+    sr_counts_sum[sr] = j + 1
     last_sr = sr
   end
 
-  sr_counts_sum = Hash.new{ |hash, key| hash[key] = 0 }
-  last_count = 0
-  srs.each do |sr|
-    last_count += sr_counts[sr]
-    sr_counts_sum[sr] = last_count
+  sr_counts_by_i = []
+  sr_counts_sum_by_i = []
+
+  sr2is.each do |sr, is|
+    sr_count_sum = sr_counts_sum[sr]
+    is.each do |i|
+      sr_counts_by_i[i] = is.length
+      sr_counts_sum_by_i[i] = sr_count_sum
+    end
   end
 
   ABS.each do |a, b|
-    x0, y0 = XYS[a - 1]
-    x1, y1 = XYS[b - 1]
+    count0 = sr_counts_sum_by_i[a - 1]
+    count_line0 = sr_counts_by_i[a - 1]
+    count1 = sr_counts_sum_by_i[b - 1]
 
-    s0 = ((0 < x0 || (x0 == 0 && 0 < y0)) ? 1 : -1)
-    r0 = (x0 == 0 ? -Float::INFINITY : -Rational(y0, x0))
-    s1 = ((0 < x1 || (x1 == 0 && 0 < y1)) ? 1 : -1)
-    r1 = (x1 == 0 ? -Float::INFINITY : -Rational(y1, x1))
-
-    sr0 = [s0, r0]
-    count0 = sr_counts_sum[sr0]
-    count_line0 = sr_counts[sr0]
-
-    sr1 = [s1, r1]
-    count1 = sr_counts_sum[sr1]
-    count_line1 = sr_counts[sr1]
-
-    if count0 == count1
-      ans = count_line0
-    elsif count0 < count1
+    if count0 <= count1
       ans = count1 - count0 + count_line0
     else
-      ans = N - (count0 - count1 + count_line1) + count_line0 + count_line1
+      ans = N - count0 + count1 + count_line0
     end
     puts ans
   end
