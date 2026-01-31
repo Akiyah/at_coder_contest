9d43f86884d069d34604b0f89af0b8a3cae28346
Akiyah <akiya.mizukoshi@gmail.com>
2026-01-31 15:54:17 +0900

    wip

--- abc442/e/main_wip.rb
+++ abc442/e/main_wip.rb
@@ -26,7 +26,6 @@ end
 
 
 def calc
-
   xyis = XYS.map.with_index { |(x, y), i| [x, y, i] }
   xyis_plus = xyis.select { |x, y, i| 0 < x || (x == 0 && 0 < y) }
   # xyis_minus = xyis.select { |x, y, i| !(0 < x || (x == 0 && 0 < y)) }
@@ -42,35 +41,53 @@ def calc
     [x, y, -1, r, i]
   end
 
+  xysris_plus.sort_by! { |(x, y, s, r, i)| r }
+  xysris_minus.sort_by! { |(x, y, s, r, i)| r }
+
   xysris = xysris_plus + xysris_minus
 
-  xysris.sort_by! { |(x, y, s, r, i)| [s, r] }
+  pp(xysris:) if $debug
 
-  sr2is = {}
-  sr2is[1] = Hash.new{ |hash, key| hash[key] = [] }
-  sr2is[-1] = Hash.new{ |hash, key| hash[key] = [] }
-  sr_counts_sum = {}
-  sr_counts_sum[1] = {}
-  sr_counts_sum[-1] = {}
+  # sr2is = {}
+  # sr2is[1] = Hash.new{ |hash, key| hash[key] = [] }
+  # sr2is[-1] = Hash.new{ |hash, key| hash[key] = [] }
+  # sr_counts_sum = {}
+  # sr_counts_sum[1] = {}
+  # sr_counts_sum[-1] = {}
 
-  xysris.each.with_index do |(x, y, s, r, i), j|
-    sr = [s, r]
-    sr2is[s][r] << i
-    sr_counts_sum[s][r] = j + 1
-  end
+  # xysris.each.with_index do |(x, y, s, r, i), j|
+  #   sr = [s, r]
+  #   sr2is[s][r] << i
+  #   sr_counts_sum[s][r] = j + 1
+  # end
 
+  # monster_counts = []
+  # monster_counts_sum = []
+
+  # sr2is.each do |s, r2is|
+  #   r2is.each do |r, is|
+  #     sr_count_sum = sr_counts_sum[s][r]
+  #     is.each do |i|
+  #       monster_counts[i] = is.length
+  #       monster_counts_sum[i] = sr_count_sum
+  #     end
+  #   end
+  # end
+
+  sum = 0
   monster_counts = []
   monster_counts_sum = []
-
-  sr2is.each do |s, r2is|
-    r2is.each do |r, is|
-      sr_count_sum = sr_counts_sum[s][r]
-      is.each do |i|
-        monster_counts[i] = is.length
-        monster_counts_sum[i] = sr_count_sum
-      end
+  xysris.chunk_while { |(x0, y0, s0, r0, i0), (x1, y1, s1, r1, i1)| s0 == s1 && r0 == r1 }.each do |line_xysris|
+    pp(line_xysris:) if $debug
+    l = line_xysris.length
+    sum += l
+    line_xysris.each do |x, y, s, r, i|
+      monster_counts[i] = l
+      monster_counts_sum[i] = sum
     end
   end
+
+  pp(monster_counts:, monster_counts_sum:) if $debug
 
   rs = ABS.map do |a, b|
     count_sum0 = monster_counts_sum[a - 1]
