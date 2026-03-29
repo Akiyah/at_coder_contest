382d9724ad5ddf9807968eb54332313c6ab09d8b
Akiyah <akiya.mizukoshi@gmail.com>
2026-03-29 23:14:00 +0900

    abc451 d ac

--- abc451/d/main.rb
+++ abc451/d/main.rb
@@ -19,115 +19,34 @@ $debug = !ARGV[0].nil?
 N = STDIN.gets.chomp.to_i
 
 
-
-def check(s, gs)
-  # pp(s:) if $debug
-  l = s.length
-  (0...l).each do |i|
-    s1 = s[0..i]
-    if gs[s1] # 先頭i文字がgsに含まれていた
-      if i == l - 1 # 残り文字数がない場合
-        return true
-      else
-        return check(s[(i + 1)..], gs)
-      end
-    end
-  end
-  false
-end
-
-
-gs = {}
 gls = {}
 (0..29).each do |i| # 2 ** 30 > 10 ** 9
-  s = (2 ** i).to_s
-  r = check(s, gs)
-
-  # pp([i, s, r]) if $debug
-
-  pp([i, s, r]) if r && $debug
-
-  if !r
-    gs[s] = true
-    l = s.length
-    gls[l] ||= []
-    gls[l] << s
-  end
+  n = 2 ** i
+  s = n.to_s
+  l = s.length
+  gls[l] ||= []
+  gls[l] << n
 end
 
-
-# pp(gs:) if $debug
-# pp(l: gs.length) if $debug
 pp(gls:) if $debug
 
 
-
-
-
-def calc_keta(l, gls) # ちょうどその桁数の良い整数の数
-  return 1 if l == 0
-
-  r = 0
-  gls.each do |l2, gs|
-    if l2 <= l
-      r += gs.length * calc_keta(l - l2, gls)
+def create_array(gls)
+  rls = {}
+  (1..9).each do |i|
+    rls[i] ||= []
+    rls[i] += gls[i]
+    (1...i).each do |j|
+      gls[j].product(rls[i - j]) { |a, b| rls[i] << a * (10 ** (i - j)) + b }
     end
   end
-  r
-end
 
-def calc_kazu(s, gls) # その数以下で同じ桁数の良い整数の数
-  l = s.length
-  return 1 if l == 0
-
-  r = 0
-  gls.each do |l2, gs|
-    if l2 <= l
-      gs.each do |g|
-        if g == s[...l2]
-          r += calc_kazu(s[l2..], gls)
-        elsif g < s[...l2]
-          r += calc_keta(l - l2, gls)
-        end
-      end
-    end
-  end
-  r
-end
-
-def calc_m(n, gls) # その数以下の良い整数の数を求める
-  # pp(n:) if $debug
-  s = n.to_s
-  l = s.length
-
-  r = 0
-  r1 = calc_kazu(s, gls)
-  # pp(r1:) if $debug
-  r += r1
-  (1...l).each do |l2|
-    r2 = calc_keta(l2, gls)
-    # pp(l2:, r2:) if $debug
-    r += r2
-  end
-
-  # pp(n:, r:) if $debug
-  r
+  rls.map { |i, rs| rs.uniq.sort }.flatten
 end
 
 
-r = (1..(10 ** 9)).bsearch { |n| N <= calc_m(n, gls) }
+rs = create_array(gls)
+pp(rs: rs[..100]) if $debug
 
-
-puts r
-
-
-
-if $debug
-  r0 = 0
-  (1..400).each do |n|
-    r = calc_m(n, gls)
-    pp(n:, r:) if r0 != r
-    r0 = r
-  end
-end
+puts rs[N - 1]
 
