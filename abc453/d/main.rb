a8e6d03bbb0e7d8981753b388674b8b547943676
Akiyah <akiya.mizukoshi@gmail.com>
2026-04-20 02:21:00 +0900

    wip (refactoring)

--- abc453/d/main.rb
+++ abc453/d/main.rb
@@ -35,52 +35,15 @@ $d2p = [
 ]
 
 def calc
-  # used = Array.new(H) { Array.new(W) { [false, false, false, false] } }
   used = Array.new(H * W) { 0 }
   s = []
-
-  pp(used:) if $debug
-  pp(used: used.map { |u| u.to_s(2) }) if $debug
 
   board = SS.map.with_index do |line, i|
     js = line.index('S')
     s = [i, js] if js
     line.tr!('S', '.')
-    syss = line.chars.map(&:to_sym)
-    syss.each.with_index do |x, j|
-      if x == :'#'
-        used[((i + 1) * W + j)] |= 0b0001 if i + 1 < H # U
-        used[((i - 1) * W + j)] |= 0b0010 if 0 < i - 1 # D
-        used[(i * W + (j - 1))] |= 0b0100 if 0 < j - 1 # R
-        used[(i * W + (j + 1))] |= 0b1000 if j + 1 < W # L
-      end
-    end
-    syss.map { |x| [x, x == :'.', x == :'o', x == :'#'] }
+    line.chars.map(&:to_sym)
   end.flatten(1)
-
-  pp(used:) if $debug
-  pp(used: used.map { |u| u.to_s(2) }) if $debug
-
-  H.times do |i|
-    used[(i * W + 0)] |= 0b1000 # L
-    used[(i * W + (W - 1))] |= 0b0100 # R
-  end
-  W.times do |j|
-    used[(0 * W + j)] |= 0b0001 # U
-    used[((H - 1) * W + j)] |= 0b0010 # D
-  end
-  # H.times do |i|
-  #   W.times do |j|
-  #     x, y, z, w = board[i * W + j]
-  #     pp(x:, y:, z:) if $debug
-  #     if w
-  #       used[((i + 1) * W + j) * 4 + 0] = true if i + 1 < H # U
-  #       used[((i - 1) * W + j) * 4 + 1] = true if 0 < i - 1 # D
-  #       used[(i * W + (j - 1)) * 4 + 2] = true if j + 1 < W # R
-  #       used[(i * W + (j + 1)) * 4 + 3] = true if 0 < j - 1 # L
-  #     end
-  #   end
-  # end
 
   pp(used:) if $debug
   pp(used: used.map { |u| u.to_s(2) }) if $debug
@@ -89,54 +52,96 @@ def calc
   si, sj = s
 
   dp = []
-  k = si * W + sj
-  (0..3).each do |d|
-    next if used[k] & (1 << d) != 0
-
-    used[k] |= (1 << d)
-
-    dp << [si, sj, d, nil]
+  if 0 <= si - 1 && used[(si - 1) * W + sj] & 0b0001 == 0
+    dp << [si - 1, sj, 0, nil]
+    used[(si - 1) * W + sj] |= 0b0001
+  end
+  if si + 1 < H && used[(si + 1) * W + sj] & 0b0010 == 0
+    dp << [si + 1, sj, 1, nil]
+    used[(si + 1) * W + sj] |= 0b0010
+  end
+  if sj + 1 < W && used[si * W + (sj + 1)] & 0b0100 == 0
+    dp << [si, sj + 1, 2, nil]
+    used[si * W + (sj + 1)] |= 0b0100
+  end
+  if 0 <= sj - 1 && used[si * W + (sj - 1)] & 0b1000 == 0
+    dp << [si, sj - 1, 3, nil]
+    used[si * W + (sj - 1)] |= 0b1000
   end
 
   pp(dp:) if $debug
+  pp(used:) if $debug
+  pp(used: used.map { |u| u.to_s(2) }) if $debug
 
   while !dp.empty?
     p_d0_parent = dp.pop
     pi, pj, d0, parent = p_d0_parent
     pp(pi:, pj:, d0:) if $debug
 
-    d0i, d0j = $d2p[d0]
-    qi, qj = pi + d0i, pj + d0j
+    # d0i, d0j = $d2p[d0]
+    # qi, qj = pi + d0i, pj + d0j
 
-    k = qi * W + qj
-    x, y, z = board[k]
-    pp(x:, y:, z:) if $debug
+    # k = qi * W + qj
+    x = board[pi * W + pj]
+    # pp(x:, y:, z:) if $debug
     if x == :G
       return [true, p_d0_parent]
-    # elsif x == :'#'
-    #   next
-    else
-      u = used[k]
-      (0..3).each do |d|
-        next unless y || (z == (d == d0))
-        # next unless (x == :'.') || ((x == :'o') == (d == d0))
-
-        # k2 = k4 + d
-
-        next if u[d] != 0
-        u |= (1 << d)
-
-        # b = used[k2]
-        # next if b
-
-        # used[k2] = true
-
-        dp << [qi, qj, d, p_d0_parent]
+    elsif x == :'#'
+      next
+    elsif x == :'.'
+      if 0 <= pi - 1 && used[(pi - 1) * W + pj] & 0b0001 == 0
+        dp << [pi - 1, pj, 0, p_d0_parent]
+        used[(pi - 1) * W + pj] |= 0b0001
       end
-      used[k] = u
+      if pi + 1 < H && used[(pi + 1) * W + pj] & 0b0010 == 0
+        dp << [pi + 1, pj, 1, p_d0_parent]
+        used[(pi + 1) * W + pj] |= 0b0010
+      end
+      if pj + 1 < W && used[pi * W + (pj + 1)] & 0b0100 == 0
+        dp << [pi, pj + 1, 2, p_d0_parent]
+        used[pi * W + (pj + 1)] |= 0b0100
+      end
+      if 0 <= pj - 1 && used[pi * W + (pj - 1)] & 0b1000 == 0
+        dp << [pi, pj - 1, 3, p_d0_parent]
+        used[pi * W + (pj - 1)] |= 0b1000
+      end
+    elsif x == :'x'
+      if 0 <= pi - 1 && used[(pi - 1) * W + pj] & 0b0001 == 0 && d0 != 0
+        dp << [pi - 1, pj, 0, p_d0_parent]
+        used[(pi - 1) * W + pj] |= 0b0001
+      end
+      if pi + 1 < H && used[(pi + 1) * W + pj] & 0b0010 == 0 && d0 != 1
+        dp << [pi + 1, pj, 1, p_d0_parent]
+        used[(pi + 1) * W + pj] |= 0b0010
+      end
+      if pj + 1 < W && used[pi * W + (pj + 1)] & 0b0100 == 0 && d0 != 2
+        dp << [pi, pj + 1, 2, p_d0_parent]
+        used[pi * W + (pj + 1)] |= 0b0100
+      end
+      if 0 <= pj - 1 && used[pi * W + (pj - 1)] & 0b1000 == 0 && d0 != 3
+        dp << [pi, pj - 1, 3, p_d0_parent]
+        used[pi * W + (pj - 1)] |= 0b1000
+      end
+    else # x == :'o'
+      if 0 <= pi - 1 && used[(pi - 1) * W + pj] & 0b0001 == 0 && d0 == 0
+        dp << [pi - 1, pj, 0, p_d0_parent]
+        used[(pi - 1) * W + pj] |= 0b0001
+      end
+      if pi + 1 < H && used[(pi + 1) * W + pj] & 0b0010 == 0 && d0 == 1
+        dp << [pi + 1, pj, 1, p_d0_parent]
+        used[(pi + 1) * W + pj] |= 0b0010
+      end
+      if pj + 1 < W && used[pi * W + (pj + 1)] & 0b0100 == 0 && d0 == 2
+        dp << [pi, pj + 1, 2, p_d0_parent]
+        used[pi * W + (pj + 1)] |= 0b0100
+      end
+      if 0 <= pj - 1 && used[pi * W + (pj - 1)] & 0b1000 == 0 && d0 == 3
+        dp << [pi, pj - 1, 3, p_d0_parent]
+        used[pi * W + (pj - 1)] |= 0b1000
+      end
     end
 
-    pp(dp: dp[0...3]) if $debug
+    pp(dp:) if $debug
 
     # pp(dp:, used_D:, used_U:, used_R:, used_L:) if $debug
   end
