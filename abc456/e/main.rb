6be1f88eabd47d19e68c1cfd8b6a02ab489b4c06
Akiyah <akiya.mizukoshi@gmail.com>
2026-05-04 12:51:52 +0900

    wip

--- abc456/e/main.rb
+++ abc456/e/main.rb
@@ -23,7 +23,6 @@ T = STDIN.gets.chomp.to_i
 def calc(n, m, uvs, w, ss)
   pp(n:, m:, uvs:, w:, ss:) if $debug
 
-
   paths = Hash.new { |hash, key| hash[key] = [] }
   uvs.each do |u, v|
     paths[u - 1] << v - 1
@@ -33,33 +32,44 @@ def calc(n, m, uvs, w, ss)
     paths[u] << u
   end
 
-
   oss = ss.map { |s| s.chars.map { |c| c == 'o' } }
   pp(paths:, oss:) if $debug
 
-  routes = Array.new(n, Set.new)
+  dp = []
+  useds = Array.new(n) { [] }
+
+  dp << []
   oss.each.with_index do |os, u|
-    routes[u] << u if os[0]
+    dp[0] << u if os[0]
   end
-  pp(routes:) if $debug
 
-  (1..w).each do |j|
-    routes2 = Array.new(n, Set.new)
-    routes.each.with_index do |us, v|
-      paths[v].each do |v2|
-        if oss[v2][j % w]
-          routes2[v2] += us
-        end
-      end
+  d = 0
+  route_data = Array.new(n) { [] }
+  route = []
+  while true
+
+    if dp[d].length == 0 # 一つ上の次に戻る
+      return false if d == 0 # 全部探索した
+      u = route.pop
+      route_data[u][(d - 1) % w] = false
+      d -= 1
+      next
     end
-    routes = routes2
-    pp(j:, routes:) if $debug
+
+    u = dp[d].pop
+    route << u
+
+    return true if route_data[u][d % w] # 同じところを通ったことがある
+    route_data[u][d % w] = true
+
+    next if useds[u][d % w] # すでに確認済みの場合
+    useds[u][d % w] = true
+
+    vs = paths[u].filter { |v| oss[v][(d + 1) % w] }
+    dp[d + 1] = vs
+    d += 1
   end
 
-  routes.each.with_index do |us, v|
-    pp(us:, v:) if $debug
-    return true if us.include?(v)
-  end
   false
 end
 
