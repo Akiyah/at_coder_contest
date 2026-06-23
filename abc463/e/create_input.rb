483abb3fc3cc22281348a0470e4f85a5f8dd2236
Akiyah <akiya.mizukoshi@gmail.com>
2026-06-23 23:07:16 +0900

    wip

---
+++ abc463/e/create_input.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+# N = 10 ** 5
+# M = 10 ** 5
+N = 2 * 10 ** 3
+M = 2 * 10 ** 3
+Y = rand(10 ** 9) + 1
+XS = N.times.map { rand(10 ** 9) + 1 }
+
+def create_uvts
+  uvts = {}
+  co = 0
+  while co < M
+    u = rand(N) + 1
+    v = rand(N) + 1
+    next if u == v
+    u, v = v, u if v < u
+    next if uvts[u] && uvts[u][v]
+
+    t = rand(10 ** 9) + 1
+    uvts[u] ||= {}
+    uvts[u][v] = t
+    co += 1
+  end
+
+  uvts
+end
+
+uvts = create_uvts
+
+puts [N, M, Y].join(' ')
+
+uvts.each do |u, vts|
+  vts.each do |v, t|
+    puts [u, v, t].join(' ')
+  end
+end
+
+puts XS.join(' ')
--- abc463/e/main.rb
+++ abc463/e/main.rb
@@ -27,51 +27,55 @@ XS = STDIN.gets.chomp.split.map(&:to_i)
 def calc
 
   paths = {}
-  M.times do |u|
+  N.times do |u|
     paths[u] ||= {}
-    M.times do |v|
-      paths[u][v] = XS[u] + XS[v] + Y
+    N.times do |v|
+      paths[u][v] = XS[u] + XS[v] + Y unless u == v
     end
   end
+  pp(paths:) if $debug
 
-  UVTS.each do |u, v, t|
+  UVTS.each do |u1, v1, t|
+    u = u1 - 1
+    v = v1 - 1
     if t < paths[u][v]
       paths[u][v] = t
       paths[v][u] = t
     end
   end
+  pp(paths:) if $debug
 
 
   pq = AcLibraryRb::PriorityQueue.new { |(u1, t1), (u2, t2)| t1 < t2 }
   pq << [0, 0] #  都市1 = start, time = 0
   ts = Array.new(N)
-  ts[0] = 0 # 都市1 = start
-  co = 1 # 計算済みの都市
+  # ts[0] = 0 # 都市1 = start
+  co = 0 # 計算済みの都市
+
+  pp(pq:) if $debug
 
   while true
-
     u, t = pq.pop
+    pp(u:, t:) if $debug
+    next if ts[u] # 計算済み
 
-    paths_u = paths[u] || {}
-    N.times do |v|
-      next if v == u
-      next if ts[v] # 訪問済み
-      t2 = paths_u[v]
-      t_new = if t2 && t2 < XS[u] + XS[v] + Y
-                t + t2
-              else
-                t + XS[u] + XS[v] + Y
-              end
-      dp << [v, t_new]
-      ts[v] = t_new
+    ts[u] = t # 訪問
+    co += 1
+    return ts if co == N
+
+    paths[u].each do |v2, t2|
+      next if ts[v2] # 訪問済み
+      pq << [v2, t + t2]
     end
   end
+  ts
 end
 
 
 
 
 
-rs = calc
-puts rs.join(' ')
+ts = calc
+pp(ts:) if $debug
+puts ts[1...].join(' ')
 
