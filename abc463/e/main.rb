812a0d3fd473851072212c3333dacebf13274c78
Akiyah <akiya.mizukoshi@gmail.com>
2026-06-25 00:03:09 +0900

    wip

--- abc463/e/main.rb
+++ abc463/e/main.rb
@@ -27,45 +27,58 @@ XS = STDIN.gets.chomp.split.map(&:to_i)
 def calc
 
   paths = {}
-  N.times do |u|
-    paths[u] ||= {}
-    N.times do |v|
-      paths[u][v] = XS[u] + XS[v] + Y unless u == v
-    end
-  end
-  pp(paths:) if $debug
+  # N.times do |u|
+  #   paths[u] ||= {}
+  #   N.times do |v|
+  #     paths[u][v] = XS[u] + XS[v] + Y unless u == v
+  #   end
+  # end
+  # pp(paths:) if $debug
 
   UVTS.each do |u1, v1, t|
     u = u1 - 1
     v = v1 - 1
-    if t < paths[u][v]
-      paths[u][v] = t
-      paths[v][u] = t
-    end
+
+    paths[u] ||= {}
+    paths[u][v] = t
+    paths[v] ||= {}
+    paths[v][u] = t
   end
   pp(paths:) if $debug
 
 
-  pq = AcLibraryRb::PriorityQueue.new { |(u1, t1), (u2, t2)| t1 < t2 }
-  pq << [0, 0] #  都市1 = start, time = 0
+  pq = AcLibraryRb::PriorityQueue.new { |(u1, t1, used1), (u2, t2, used2)| t1 == t2 ? used1 < used2 : t1 < t2 }
+  pq << [0, 0, 0] #  都市1 = start, time = 0, used = false
   ts = Array.new(N)
-  # ts[0] = 0 # 都市1 = start
   co = 0 # 計算済みの都市
 
   pp(pq:) if $debug
 
   while true
-    u, t = pq.pop
-    pp(u:, t:) if $debug
+    u, t, used = pq.pop
+    pp(u:, t:, used:) if $debug
     next if ts[u] # 計算済み
 
     ts[u] = t # 訪問
     co += 1
     return ts if co == N
 
-    paths[u].each do |v2, t2|
-      next if ts[v2] # 訪問済み
-      pq << [v2, t + t2]
+    if paths[u]
+      paths[u].each do |v2, t2|
+        next if ts[v2] # 訪問済み
+        pq << [v2, t + t2, 0]
+      end
+    end
+
+    if used == 0
+      N.times do |v2|
+        next if v2 == u
+        next if ts[v2] # 訪問済み
+
+        t2 = XS[u] + XS[v2] + Y
+
+        pq << [v2, t + t2, 1]
+      end
     end
   end
   ts
