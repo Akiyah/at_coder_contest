e1d0311cab4a2c4c19a774c2e7f2b4796aa307de
Akiyah <akiya.mizukoshi@gmail.com>
2026-08-01 11:12:09 +0900

    wip

--- abc463/e/test.rb
+++ abc463/e/test.rb
@@ -8,18 +8,18 @@
 
 100.times do |i|
 
-  n = 5
-  m = 5
-  y = rand(100)
+  n = 4
+  m = 4
+  y = rand(10) + 1
   uvts = {}
   while uvts.length < m
-    u = rand(n)
-    v = rand(n)
-    t = rand(100)
+    u = rand(n) + 1
+    v = rand(n) + 1
+    t = rand(10) + 1
     uvts[[u, v]] ||= t
   end
-  xs = n.times.map { rand(100) }
-  
+  xs = n.times.map { rand(10) + 1 }
+
   File.open('test/sample.in', 'w') do |f|
     f.puts [n, m, y].join(' ')
     uvts.each do |uv, t|
